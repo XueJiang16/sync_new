301fabac7e15710571ff1a93d31338ec3d62ef96
jx <763565607@qq.com>
2022-07-26 23:16:09 +0800

    jx commit

--- mmclassification_dev/mmcls/models/backbones/resnet.py
+++ mmclassification_dev/mmcls/models/backbones/resnet.py
@@ -282,7 +282,7 @@ class Bottleneck(BaseModule):
 
 class RandomBlock(BaseModule):
 
-    def __init__(self, k, non_linear='relu'):
+    def __init__(self, k, non_linear='l_relu'):
         super(RandomBlock, self).__init__()
         self.k = k
         if non_linear == 'identity':
