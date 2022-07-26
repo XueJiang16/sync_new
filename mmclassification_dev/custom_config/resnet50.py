4e0c3fa89e02e057f3e19f040c56ad888cbdf4a9
jx <763565607@qq.com>
2022-07-26 22:17:42 +0800

    jx commit

--- mmclassification_dev/custom_config/resnet50.py
+++ mmclassification_dev/custom_config/resnet50.py
@@ -25,9 +25,9 @@ model = dict(
             num_stages=4,
             out_indices=(3,),
             style='pytorch',
-            random_block=[1,1,1],
-            random_block_k=[3,5,7],
-            random_block_location=[0,1,2],  # 0:C2 1:C3 2:C4 3:C5
+            random_block=[1,1],
+            random_block_k=[2.5,2.5],
+            random_block_location=[1,2],  # 0:C2 1:C3 2:C4 3:C5
         ),
         neck=dict(type='GlobalAveragePooling'),
         head=dict(
