18147c6a31b9284511fbfb7cc32e95c239bae1cb
jx <763565607@qq.com>
2022-07-27 20:24:13 +0800

    jx commit

--- mmclassification_dev/custom_config/resnet101_nest_cfg.py
+++ mmclassification_dev/custom_config/resnet101_nest_cfg.py
@@ -47,7 +47,7 @@ for train_data, target_file, ckpt in zip(*multi_cfg_list):
             )
         ),
         data = dict(
-            samples_per_gpu=256,
+            samples_per_gpu=64,
             workers_per_gpu=4,
             id_data=dict(
                 name='ImageNet',
