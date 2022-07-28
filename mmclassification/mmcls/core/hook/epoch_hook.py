35ca5551d08eb8c3afb2ad2cca537f798b409a3a
jx <763565607@qq.com>
2022-07-28 17:10:38 +0800

    jx commit

--- mmclassification/mmcls/core/hook/epoch_hook.py
+++ mmclassification/mmcls/core/hook/epoch_hook.py
@@ -13,6 +13,4 @@ class EpochHook(Hook):
             runner (obj:`EpochBasedRunner`): Epoch based Runner.
         """
         curr_epoch = runner.epoch
-        print(runner.model)
-        assert False
-        runner.model.head.loss.set_drw_epoch(curr_epoch)
+        runner.model.module.head.loss.set_drw_epoch(curr_epoch)
