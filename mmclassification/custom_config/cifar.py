ac09b046d87ecd27e8c089bf13267616710ad7be
jx <763565607@qq.com>
2022-07-28 20:15:01 +0800

    jx commit

--- mmclassification/custom_config/cifar.py
+++ mmclassification/custom_config/cifar.py
@@ -59,7 +59,7 @@ optimizer_config = dict(grad_clip=None)
 lr_config = dict(policy='step', step=[100, 150])
 runner = dict(type='EpochBasedRunner', max_epochs=200)
 checkpoint_config = dict(interval=20)
-log_config = dict(interval=200, hooks=[dict(type='TextLoggerHook')])
+log_config = dict(interval=30, hooks=[dict(type='TextLoggerHook')])
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 load_from = None
