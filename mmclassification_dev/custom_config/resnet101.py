79836b908d707c593c2a2106040f2c6f18d58f47
jx <763565607@qq.com>
2022-07-28 21:15:13 +0800

    jx commit

--- mmclassification_dev/custom_config/resnet101.py
+++ mmclassification_dev/custom_config/resnet101.py
@@ -1,4 +1,4 @@
-method_name = 'Cosine'
+method_name = 'GradNormBatch'
 model_name = 'resnet101'
 train_dataset = 'LT_a8'
 custom_name = None
@@ -14,6 +14,7 @@ model = dict(
     num_classes=1000,
     # temperature=1,
     target_file=training_file,
+    target_noise=0.1,
     classifier=dict(
         type='ImageClassifier',
         init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/ood_ckpt_other/LT_a8/epoch_100.pth'),
