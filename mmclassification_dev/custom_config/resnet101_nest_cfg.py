cfb5375d5c327e8271c38ed8bd48dedde4d5de2f
jx <763565607@qq.com>
2022-07-28 20:29:07 +0800

    jx commit

--- mmclassification_dev/custom_config/resnet101_nest_cfg.py
+++ mmclassification_dev/custom_config/resnet101_nest_cfg.py
@@ -4,7 +4,7 @@ pipline = [dict(type='Collect', keys=['img', 'type'])]
 
 train_dataset_list = ['LT_a{}'.format(x) for x in range(2, 9)]
 # target_file_list = ['/data/csxjiang/meta/train_LT_a{}.txt'.format(x) for x in range(2, 9)]
-train_dataset_list = [None]*8
+target_file_list = [None]*8
 checkpoint_list = ['/data/csxjiang/ood_ckpt/ood_ckpt_other/LT_a{}/epoch_100.pth'.format(x)
                    for x in range(2, 9)]
 multi_cfg_list = [train_dataset_list, target_file_list, checkpoint_list]
