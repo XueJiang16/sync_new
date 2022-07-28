90b4adeae3853653e259b4049d427bc7e2548557
jx <763565607@qq.com>
2022-07-28 22:40:55 +0800

    jx commit

--- mmclassification_dev/mmcls/models/ood/utils.py
+++ mmclassification_dev/mmcls/models/ood/utils.py
@@ -25,7 +25,9 @@ def add_noise(target, std_ratio):
     std_target = target.std()
     std_noise = std_ratio * std_target
     noise = torch.randn_like(target) * std_noise
+    print(target[:10])
     target += noise
+    print(target[:10])
     target[target < 0] = 0
     target = target / target.sum()
     return target
