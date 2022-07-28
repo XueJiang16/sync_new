3e996600fc80c77addc935e5d2346e68e091498e
jx <763565607@qq.com>
2022-07-28 20:15:09 +0800

    jx commit

--- mmclassification/custom_config/cifar.py
+++ mmclassification/custom_config/cifar.py
@@ -51,7 +51,7 @@ data = dict(
         test_mode=True))
 
 
-evaluation = dict(interval=20, metric='accuracy')
+evaluation = dict(interval=200, metric='accuracy')
 # optimizer
 optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
