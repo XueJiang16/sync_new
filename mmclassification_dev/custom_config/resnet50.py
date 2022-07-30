b86bb2cba8ef4783a1189eabd3fe73bc7462bbb1
jx <763565607@qq.com>
2022-07-30 21:16:05 +0800

    jx commit

--- mmclassification_dev/custom_config/resnet50.py
+++ mmclassification_dev/custom_config/resnet50.py
@@ -1,4 +1,4 @@
-method_name = 'MSPCustom'
+method_name = 'ODINCustom'
 model_name = 'resnet50'
 train_dataset = 'LT_a8'
 custom_name = None
@@ -43,7 +43,7 @@ model = dict(
 # pipline =[dict(type='Collect', keys=['img'])]
 pipline =[dict(type='Collect', keys=['img', 'type'])]
 data = dict(
-    samples_per_gpu=128,
+    samples_per_gpu=32,
     workers_per_gpu=4,
     id_data=dict(
         name='ImageNet',
