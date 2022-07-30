279842fe555a5d9dd5df171cf798eb6607f8730e
jx <763565607@qq.com>
2022-07-31 00:36:43 +0800

    jx commit

--- mmclassification_dev/custom_config/resnet101.py
+++ mmclassification_dev/custom_config/resnet101.py
@@ -36,7 +36,7 @@ model = dict(
 # pipline =[dict(type='Collect', keys=['img'])]
 pipline =[dict(type='Collect', keys=['img', 'type'])]
 data = dict(
-    samples_per_gpu=256,
+    samples_per_gpu=64,
     workers_per_gpu=4,
     id_data=dict(
         name='ImageNet',
