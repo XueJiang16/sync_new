22a1f6fb5a5849bcfd411558e8c7c2d9a7093785
jx <763565607@qq.com>
2022-08-01 22:41:15 +0800

    jx commit

--- gradnorm_ood/run.sh
+++ gradnorm_ood/run.sh
@@ -32,14 +32,14 @@ list_method=('MSP')
 #export CUDA_VISIBLE_DEVICES=1
 #bash ./scripts/tune_mahalanobis.sh ./ood_ckpt/mmcls_offical/resnet101_8xb32_in1k_20210831-539c63f8.pth \
 #    ./maha_ckpts /data/csxjiang/meta/train_labeled.txt
-#bash ./scripts/tune_mahalanobis.sh ./ood_ckpt/ood_ckpt_other/LT_a8/epoch_100.pth \
-#    ./maha_ckpts/test /data/csxjiang/meta/train_labeled_1percent.txt
-for dataset in ${list_dataset[*]}
-do
-bash  ./custom_test_v3_.sh Mahalanobis $dataset ./ood_ckpt/ood_ckpt_other/LT_a8/epoch_100.pth \
-      checkpoint0801/test_maha /data/csxjiang/meta/train_LT_a8.txt 0 \
-      --mahalanobis_param_path ./maha_ckpts/test/tune_mahalanobis/
-done
+bash ./scripts/tune_mahalanobis.sh ./ood_ckpt/ood_ckpt_other/LT_a8/epoch_100.pth \
+    ./maha_ckpts/test /data/csxjiang/meta/train_labeled_1percent.txt
+#for dataset in ${list_dataset[*]}
+#do
+#bash  ./custom_test_v3_.sh Mahalanobis $dataset ./ood_ckpt/ood_ckpt_other/LT_a8/epoch_100.pth \
+#      checkpoint0801/test_maha /data/csxjiang/meta/train_LT_a8.txt 0 \
+#      --mahalanobis_param_path ./maha_ckpts/test/tune_mahalanobis/
+#done
 # for method in ${list_method[*]}
 # do
 # for dataset in ${list_dataset[*]}
