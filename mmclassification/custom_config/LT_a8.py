1591eb51931af2ca4b2a9274af4ba7cd7aa643b2
jx <763565607@qq.com>
2022-08-03 18:18:04 +0800

    jx commit

--- mmclassification/custom_config/LT_a8.py
+++ mmclassification/custom_config/LT_a8.py
@@ -71,7 +71,7 @@ data = dict(
             dict(type='Collect', keys=['img'])
         ]))
 # custom_hooks = [dict(type='EpochHook')]
-evaluation = dict(interval=10, metric='accuracy')
+evaluation = dict(interval=50, metric='accuracy')
 optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 lr_config = dict(policy='step', step=[30, 60, 90])
