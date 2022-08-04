f9c6faaa3657b9d109cb6c06136fe76fed7423a4
jx <763565607@qq.com>
2022-08-04 20:13:03 +0800

    jx commit

--- mmclassification_dev/mmcls/models/ood/msp.py
+++ mmclassification_dev/mmcls/models/ood/msp.py
@@ -63,8 +63,9 @@ class MSPCustom(BaseModule):
             out_softmax = torch.nn.functional.softmax(outputs, dim=1)
             targets = self.target
             confs = out_softmax - targets
-            cos_sim = (1-out_softmax * targets) / 2
+            cos_sim = -out_softmax * targets
             cos_sim = cos_sim.sum(1) / (torch.norm(out_softmax, dim=1) * torch.norm(targets, dim=1))
+            cos_sim = (1 + cos_sim) / 2
             cos_sim = cos_sim.unsqueeze(1)
             confs = confs * cos_sim
             confs, _ = torch.max(confs, dim=-1)
