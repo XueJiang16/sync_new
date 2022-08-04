945a206340f2b244bc78105f9ba00720901b88bc
jx <763565607@qq.com>
2022-08-04 20:45:22 +0800

    jx commit

--- mmclassification/start.sh
+++ mmclassification/start.sh
@@ -22,5 +22,5 @@
 # bash ./tools/dist_train.sh custom_config/resnet50.py 8
 #bash ./tools/dist_test.sh custom_config/resnet101_imagenet.py /data/csxjiang/ood_ckpt/pytorch_official/resnet50_custom.pth 2 --metrics accuracy
 #bash ./tools/dist_train.sh custom_config/resnet101_imagenet.py 2
-bash ./tools/dist_train.sh custom_config/LT_a8.py 2
-#bash ./tools/dist_train.sh custom_config/cifar.py 2
+#bash ./tools/dist_train.sh custom_config/LT_a8.py 2
+bash ./tools/dist_train.sh custom_config/cifar.py 2
