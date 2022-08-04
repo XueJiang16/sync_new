9739cd3d9edfddf9841dd50e1de4177462883b2f
jx <763565607@qq.com>
2022-08-04 20:40:38 +0800

    jx commit

--- mmclassification_dev/custom_config/cifar10.py
+++ mmclassification_dev/custom_config/cifar10.py
@@ -28,7 +28,7 @@ model = dict(
         neck=dict(type='GlobalAveragePooling'),
         head=dict(
             type='LinearClsHead',
-            num_classes=100,
+            num_classes=10,
             in_channels=512,
             loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
             topk=(1, 5))))
