71326d3cdedec0a39ec71edb1f5524766552b408
jx <763565607@qq.com>
2022-08-04 20:50:35 +0800

    jx commit

--- mmclassification_dev/mmcls/models/ood/gradnorm.py
+++ mmclassification_dev/mmcls/models/ood/gradnorm.py
@@ -72,17 +72,19 @@ class GradNormBatch(BaseModule):
             if os.environ['LOCAL_RANK'] == '0':
                 print("*******DEBUG MODE********")
         if target_file is not None:
-            cls_idx = []
-            with open(target_file, 'r') as f:
-                for line in f.readlines():
-                    segs = line.strip().split(' ')
-                    cls_idx.append(int(segs[-1]))
-            cls_idx = np.array(cls_idx, dtype='int')
-            label_stat = Counter(cls_idx)
-            cls_num = [-1 for _ in range(num_classes)]
-            for i in range(num_classes):
-                cat_num = int(label_stat[i])
-                cls_num[i] = cat_num
+            # cls_idx = []
+            # with open(target_file, 'r') as f:
+            #     for line in f.readlines():
+            #         segs = line.strip().split(' ')
+            #         cls_idx.append(int(segs[-1]))
+            # cls_idx = np.array(cls_idx, dtype='int')
+            # label_stat = Counter(cls_idx)
+            # cls_num = [-1 for _ in range(num_classes)]
+            # for i in range(num_classes):
+            #     cat_num = int(label_stat[i])
+            #     cls_num[i] = cat_num
+            cls_num = [5000, 2997, 1796, 1077, 645, 387, 232, 139, 83, 50]
+
             target = cls_num / np.sum(cls_num)
             self.target = torch.tensor(target).to("cuda:{}".format(self.local_rank)).unsqueeze(0)
         else:
