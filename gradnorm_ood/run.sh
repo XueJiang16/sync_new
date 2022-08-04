872827e4f74e580726355543d938e83ce2d90a92
jx <763565607@qq.com>
2022-08-04 20:14:52 +0800

    jx commit

--- gradnorm_ood/run.sh
+++ gradnorm_ood/run.sh
@@ -32,15 +32,15 @@ list_dataset=('SUN' 'Places' 'Textures' 'iNaturalist')
 #export CUDA_VISIBLE_DEVICES=1
 #bash ./scripts/tune_mahalanobis.sh ./ood_ckpt/mmcls_offical/resnet101_8xb32_in1k_20210831-539c63f8.pth \
 #    ./maha_ckpts /data/csxjiang/meta/train_labeled.txt
-for i in {6,7}
+for i in {2,3,4,5,6,7}
 do
-#bash ./scripts/tune_mahalanobis.sh ./ood_ckpt/ood_ckpt_other/LT_a${i}/epoch_100.pth \
-#    ./maha_ckpts/a${i} /data/csxjiang/meta/train_LT_a${i}.txt
+bash ./scripts/tune_mahalanobis.sh ./ood_ckpt/ood_ckpt_other/LT_a${i}/epoch_100.pth \
+    ./maha_ckpts/a${i} /data/csxjiang/meta/train_LT_a${i}.txt
 for dataset in ${list_dataset[*]}
 do
 bash  ./custom_test_v3_.sh Mahalanobis $dataset ./ood_ckpt/ood_ckpt_other/LT_a${i}/epoch_100.pth \
       checkpoint0801/maha_a${i} /data/csxjiang/meta/train_LT_a${i}.txt 0 \
-      --mahalanobis_param_path ./maha_ckpts/a8/tune_mahalanobis/
+      --mahalanobis_param_path ./maha_ckpts/a${i}/tune_mahalanobis/
 done
 done
 # for method in ${list_method[*]}
