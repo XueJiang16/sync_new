250682dc688f2af8b676bbd9216f917222d9c555
jx <763565607@qq.com>
2022-08-06 17:08:10 +0800

    jx commit

--- mmclassification_dev/mmcls/models/ood/cosine.py
+++ mmclassification_dev/mmcls/models/ood/cosine.py
@@ -17,17 +17,18 @@ class Cosine(BaseModule):
         self.num_classes = num_classes
         self.criterion = torch.nn.Softmax(dim=-1).to("cuda:{}".format(self.local_rank))
         if target_file is not None:
-            cls_idx = []
-            with open(target_file, 'r') as f:
-                for line in f.readlines():
-                    segs = line.strip().split(' ')
-                    cls_idx.append(int(segs[-1]))
-            cls_idx = np.array(cls_idx, dtype='int')
-            label_stat = Counter(cls_idx)
-            cls_num = [-1 for _ in range(num_classes)]
-            for i in range(num_classes):
-                cat_num = int(label_stat[i])
-                cls_num[i] = cat_num
+            # cls_idx = []
+            # with open(target_file, 'r') as f:
+            #     for line in f.readlines():
+            #         segs = line.strip().split(' ')
+            #         cls_idx.append(int(segs[-1]))
+            # cls_idx = np.array(cls_idx, dtype='int')
+            # label_stat = Counter(cls_idx)
+            # cls_num = [-1 for _ in range(num_classes)]
+            # for i in range(num_classes):
+            #     cat_num = int(label_stat[i])
+            #     cls_num[i] = cat_num
+            cls_num = [5000, 2997, 1796, 1077, 645, 387, 232, 139, 83, 50]
             target = cls_num / np.sum(cls_num)
             self.target = torch.tensor(target).to("cuda:{}".format(self.local_rank)).unsqueeze(0)
         else:
