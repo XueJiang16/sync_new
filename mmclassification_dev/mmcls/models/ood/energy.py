36a9ccdde276d715180207ae7c705788f2db20af
jx <763565607@qq.com>
2022-08-09 17:14:16 +0800

    jx commit

--- mmclassification_dev/mmcls/models/ood/energy.py
+++ mmclassification_dev/mmcls/models/ood/energy.py
@@ -104,9 +104,9 @@ class EnergyCustom(BaseModule):
             sim = -softmax_output * targets
             sim = sim.sum(1) / (torch.norm(softmax_output, dim=1) * torch.norm(targets, dim=1))
             tmp = -targets * torch.nn.functional.log_softmax(outputs)
+            tmp = tmp / tmp.sum(1)
             print(tmp[0])
             assert False
-            tmp = tmp / tmp.sum(1)
             kl_score = torch.mean(tmp, dim=-1)
 
 
