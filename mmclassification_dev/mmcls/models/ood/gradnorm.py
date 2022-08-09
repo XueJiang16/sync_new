f1d2141f257235109bb48cdf44efb3e0a8118fd2
jx <763565607@qq.com>
2022-08-09 17:10:09 +0800

    jx commit

--- mmclassification_dev/mmcls/models/ood/gradnorm.py
+++ mmclassification_dev/mmcls/models/ood/gradnorm.py
@@ -223,8 +223,9 @@ class KLDiv(GradNorm):
             outputs = self.classifier(return_loss=False, softmax=False, post_process=False, **input)
             targets = self.target
             outputs = outputs / self.temperature
-            kl_score = torch.mean(-targets * self.logsoftmax(outputs), dim=-1)
-            print(kl_score.shape)
+            tmp = -targets * self.logsoftmax(outputs)
+            kl_score = torch.mean(tmp, dim=-1)
+            print(tmp.shape)
             assert False
         return kl_score, type
 
