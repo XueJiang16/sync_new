eef53c9be9c2cb474e696157793953eed0f4c577
jx <763565607@qq.com>
2022-08-13 21:25:24 +0800

    jx commit

--- knn-ood-master/demo_imagenet.sh
+++ knn-ood-master/demo_imagenet.sh
@@ -1,3 +1,3 @@
 
-python feat_extract_largescale.py --in-dataset imagenet  --out-datasets inat sun50 places50 dtd  --name resnet50  --model-arch resnet50
-python run_imagenet.py --in-dataset imagenet  --out-datasets inat sun50 places50 dtd  --name resnet50  --model-arch resnet50
+python feat_extract_largescale.py --in-dataset imagenet_lta  --out-datasets inat sun50 places50 dtd  --name resnet50  --model-arch resnet50
+python run_imagenet.py --in-dataset imagenet_lta --out-datasets inat sun50 places50 dtd  --name resnet50  --model-arch resnet50
