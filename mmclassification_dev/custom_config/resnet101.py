fb484a617f921c2b6beb20e54f027fdfe0c89bc2
jx <763565607@qq.com>
2022-08-13 22:19:07 +0800

    jx commit

--- mmclassification_dev/custom_config/resnet101.py
+++ mmclassification_dev/custom_config/resnet101.py
@@ -1,4 +1,4 @@
-method_name = 'Energy'
+method_name = 'ODIN'
 model_name = 'resnet101'
 train_dataset = 'balance_10'
 custom_name = None
@@ -37,7 +37,7 @@ model = dict(
 # pipline =[dict(type='Collect', keys=['img'])]
 pipline =[dict(type='Collect', keys=['img', 'type'])]
 data = dict(
-    samples_per_gpu=256,
+    samples_per_gpu=32,
     workers_per_gpu=4,
     id_data=dict(
         name='ImageNet',
