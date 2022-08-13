99fda7a130bca33b3dc7df0064fee3fe3ac048ba
jx <763565607@qq.com>
2022-08-13 22:53:31 +0800

    jx commit

--- mmclassification_dev/custom_config/resnet101.py
+++ mmclassification_dev/custom_config/resnet101.py
@@ -1,6 +1,6 @@
 # method_list = ["GradNormBatch", "MSP", "Energy", "ODIN"]
 method_list = ["GradNormBatch", "MSPCustom", "EnergyCustom", "ODINCustom"]
-method_name = method_list[1]
+method_name = method_list[2]
 model_name = 'resnet101'
 train_dataset = 'a8'
 custom_name = None
