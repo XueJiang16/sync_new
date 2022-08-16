050861f191cee2da05345a23928924c26cfdc8f4
jx <763565607@qq.com>
2022-08-16 23:32:19 +0800

    jx commit

--- mmclassification_dev/mmcls/models/backbones/resnet.py
+++ mmclassification_dev/mmcls/models/backbones/resnet.py
@@ -302,6 +302,7 @@ class RandomBlock(BaseModule):
         # noise = (torch.rand_like(x) - 0.5) / self.k  # (B,C,H,W)
         # patch_noise = noise[0,0,0:3,0:3]
         patch_noise = torch.linspace(-0.5,0.5,9).to("cuda:{}".format(self.local_rank))
+        patch_noise = patch_noise / self.k
         # patch_noise = patch_noise.flatten()
         repeat_num = int(H*W/9) + 1
         patch_noise = torch.cat([patch_noise]*repeat_num)
