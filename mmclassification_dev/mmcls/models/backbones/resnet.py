c623ce71610189c116d161672bcda6fa0cbad1a9
jx <763565607@qq.com>
2022-08-16 21:59:10 +0800

    jx commit

--- mmclassification_dev/mmcls/models/backbones/resnet.py
+++ mmclassification_dev/mmcls/models/backbones/resnet.py
@@ -297,8 +297,8 @@ class RandomBlock(BaseModule):
         # (torch.rand_like(x) - 0.5) ~ U[-0.5, 0.5)
         # print("Signal norm:", x.abs().mean())
         noise = (torch.rand_like(x) - 0.5) / self.k  # (B,C,H,W)
-        noise = noise[0,0]
-        noise = noise.unsqueeze(0)
+        noise = noise[:,0]
+        # noise = noise.unsqueeze(0)
         noise = noise.unsqueeze(0)
         out = x+noise
         out = self.non_linear(out)
