6eb27d7c9776e99f3b18ffa9a76b5056280c1f87
jx <763565607@qq.com>
2022-08-17 23:21:07 +0800

    jx commit

--- mmclassification_dev/custom_config/resnet50.py
+++ mmclassification_dev/custom_config/resnet50.py
@@ -1,5 +1,5 @@
 method_list = ['MSP', 'ODIN', 'Energy', 'GradNormBatch']
-method_name = method_list[2]
+method_name = method_list[1]
 model_name = 'resnet50'
 train_dataset = 'Balance'
 custom_name = None
