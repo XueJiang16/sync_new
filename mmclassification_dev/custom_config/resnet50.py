1a340428ea1d17662ba636b1747c3055321112c1
jx <763565607@qq.com>
2022-08-17 23:35:06 +0800

    jx commit

--- mmclassification_dev/custom_config/resnet50.py
+++ mmclassification_dev/custom_config/resnet50.py
@@ -7,7 +7,7 @@ if custom_name is not None:
     readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
 else:
     readable_name ='{}_{}_{}'.format(method_name, model_name, train_dataset)
-quick_test = True
+quick_test = False
 training_file = None
 
 model = dict(
