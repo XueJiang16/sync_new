2a81dce0a259801e10c03b7799160ad136be90eb
jx <763565607@qq.com>
2022-08-18 23:45:00 +0800

    jx commit

---
+++ mmclassification_dev/custom_config/threshold_act.py
@@ -0,0 +1,99 @@
+method_name = 'ThresholdActivation'
+model_name = 'resnet50'
+train_dataset = 'Balance'
+custom_name = None
+if custom_name is not None:
+    readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
+else:
+    readable_name ='{}_{}_{}'.format(method_name, model_name, train_dataset)
+quick_test = False
+
+model = dict(
+    type=method_name,
+    classifier=dict(
+        type='ImageClassifier',
+        init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/pytorch_official/resnet50_custom.pth'),
+        backbone=dict(
+            type='ResNet',
+            depth=50,
+            num_stages=4,
+            out_indices=(3,),
+            style='pytorch',
+            random_block=[1],
+            random_block_k=[0.1],
+            random_block_location=[2],  # 0:C2 1:C3 2:C4 3:C5
+        ),
+        neck=dict(type='GlobalAveragePooling'),
+        head=dict(
+            type='LinearClsHead',
+            num_classes=1000,
+            in_channels=2048,
+            loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+            topk=(1, 5))
+    )
+)
+# pipline =[dict(type='Collect', keys=['img'])]
+pipline =[dict(type='Collect', keys=['img', 'type'])]
+data = dict(
+    samples_per_gpu=256 if method_name is not 'ODIN' else 32,
+    workers_per_gpu=4,
+    id_data=dict(
+        name='ImageNet',
+        type='TxtDataset',
+        path='/data/csxjiang/val',
+        data_ann='/data/csxjiang/meta/val_labeled.txt',
+        pipeline=pipline,
+        len_limit=5000 if quick_test else -1,
+        train_label=None,
+    ),
+    # id_data=dict(
+    #     type='JsonDataset',
+    #     path='/data/csxjiang/',
+    #     data_ann='/data/csxjiang/ood_data/inat/val2018.json',
+    #     pipeline=[
+    #         dict(type='LoadImageFromFile'),
+    #         dict(type='Resize', size=480),
+    #         dict(
+    #             type='Normalize',
+    #             mean=[123.675, 116.28, 103.53],
+    #             std=[58.395, 57.12, 57.375],
+    #             to_rgb=True),
+    #         dict(type='ImageToTensor', keys=['img']),
+    #         dict(type='Collect', keys=['img'])
+    #     ]),
+    ood_data=[
+        dict(
+            name='iNaturalist',
+            type='FolderDataset',
+            path='/data/csxjiang/ood_data/iNaturalist/images',
+            pipeline=pipline,
+            len_limit=1000 if quick_test else -1,
+        ),
+        dict(
+            name='SUN',
+            type='FolderDataset',
+            path='/data/csxjiang/ood_data/SUN/images',
+            pipeline=pipline,
+            len_limit=1000 if quick_test else -1,
+        ),
+        dict(
+            name='Places',
+            type='FolderDataset',
+            path='/data/csxjiang/ood_data/Places/images',
+            pipeline=pipline,
+            len_limit=1000 if quick_test else -1,
+        ),
+        dict(
+            name='Textures',
+            type='FolderDataset',
+            path='/data/csxjiang/ood_data/Textures/dtd/images_collate',
+            pipeline=pipline,
+            len_limit=1000 if quick_test else -1,
+        ),
+    ],
+
+)
+dist_params = dict(backend='nccl')
+log_level = 'CRITICAL'
+# log_level = 'INFO'
+work_dir = './results_th_act/'
