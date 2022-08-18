967339eaca0d0d2ced609bd2f024f8594b6ced39
jx <763565607@qq.com>
2022-08-19 00:10:10 +0800

    jx commit

--- mmclassification_dev/custom_config/threshold_act.py
+++ mmclassification_dev/custom_config/threshold_act.py
@@ -6,7 +6,7 @@ if custom_name is not None:
     readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
 else:
     readable_name ='{}_{}_{}'.format(method_name, model_name, train_dataset)
-quick_test = False
+quick_test = True
 
 model = dict(
     type=method_name,
