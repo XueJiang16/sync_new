6eeb69eed0669a248fb3b0c6149caed8974f4e6f
jx <763565607@qq.com>
2022-08-31 23:53:18 +0800

    jx commit

--- mmclassification_dev/custom_config/vis_th_act.py
+++ mmclassification_dev/custom_config/vis_th_act.py
@@ -21,7 +21,7 @@ model = dict(
             type='ResNet',
             depth=50,
             num_stages=4,
-            out_indices=(3,),
+            out_indices=(2,3,),
             style='pytorch'),
         neck=dict(type='GlobalAveragePooling'),
         head=dict(
