1609def066e886096060430a5a470c2c89aeb82c
jx <763565607@qq.com>
2022-09-01 23:51:50 +0800

    cam_heatmap-jx commit

--- mmclassification_dev/mmcls/models/ood/feature_vis.py
+++ mmclassification_dev/mmcls/models/ood/feature_vis.py
@@ -53,13 +53,13 @@ class FeatureVis(BaseModule):
                 mid_path = 'OOD'
             else:
                 mid_path = 'ID'
-            out_dir = os.path.join('./vis_cam_features_orig/', mid_path)
+            out_dir = os.path.join('./vis_cam_features/', mid_path)
             os.makedirs(out_dir, exist_ok=True)
             _, C4_features = self.classifier(return_loss=False, softmax=False, post_process=False,
                                              require_backbone_features_idx='0', **input)
-            # k = 0.1
-            # C4_features[C4_features<k] = 0
-            # C4_features[C4_features>=k] = 1
+            k = 0.1
+            C4_features[C4_features<k] = 0
+            C4_features[C4_features>=k] = 1
             C4_features = C4_features.mean(1).cpu().numpy()
             C4_features_std = (C4_features - C4_features.min((1,2))[:,None,None]) / (C4_features.max((1,2))-C4_features.min((1,2)))[:,None,None]
             for i in range(len(filenames)):
