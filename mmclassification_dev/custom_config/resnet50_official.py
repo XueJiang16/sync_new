a2f0a6bdbab79f524f362469dd4c08af9e64253a
jx <763565607@qq.com>
2022-09-14 19:28:52 +0800

    dice-jx commit

--- mmclassification_dev/custom_config/resnet50_official.py
+++ mmclassification_dev/custom_config/resnet50_official.py
@@ -41,7 +41,7 @@ model = dict(
             loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
             topk=(1, 5),
             info=info,
-            p=70,)
+            p=0.7,)
     )
 )
 pipline =[
