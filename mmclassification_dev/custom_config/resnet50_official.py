b7064c107dfe0284b011e4104af71cbbf65f4ea8
jx <763565607@qq.com>
2022-09-14 18:02:25 +0800

    dice-jx commit

--- mmclassification_dev/custom_config/resnet50_official.py
+++ mmclassification_dev/custom_config/resnet50_official.py
@@ -6,7 +6,7 @@ if custom_name is not None:
     readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
 else:
     readable_name ='{}_{}_{}'.format(method_name, model_name, train_dataset)
-quick_test = True
+quick_test = False
 # training_file = '/data/csxjiang/meta/train_labeled.txt'
 model = dict(
     type=method_name,
