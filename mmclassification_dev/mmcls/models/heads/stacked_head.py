3756bd7d121e934a0bfe5c810f285eaf77c924fa
jx <763565607@qq.com>
2022-09-17 23:39:54 +0800

    dice-jx commit

--- mmclassification_dev/mmcls/models/heads/stacked_head.py
+++ mmclassification_dev/mmcls/models/heads/stacked_head.py
@@ -220,8 +220,8 @@ class DiceStackedLinearClsHead(StackedLinearClsHead):
             if self.masked_w is None:
                 self.calculate_mask_weight()
             vote = x[:, None, :] * self.masked_w
-            if self.fc.bias is not None:
-                cls_score = vote.sum(2) + self.fc.bias
+            if self.fc.fc.bias is not None:
+                cls_score = vote.sum(2) + self.fc.fc.bias
             else:
                 cls_score = vote.sum(2)
 
