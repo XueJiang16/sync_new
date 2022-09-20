5b87297b9b6a4644eb23d0d9849eac3f86775585
jx <763565607@qq.com>
2022-09-20 16:28:36 +0800

    dice-jx commit

--- gradnorm_ood/run.sh
+++ gradnorm_ood/run.sh
@@ -45,12 +45,12 @@ list_dataset=('SUN' 'Places' 'Textures' 'iNaturalist')
 #done
 for i in {1,2,3}
 do
-bash ./scripts/tune_mahalanobis.sh /data/csxjiang/ood_ckpt/ckpt/LT_repeat${i}_a8/epoch_100.pth \
-    ./maha_ckpts/LT_repeat${i}_a8 /data/csxjiang/meta/train_LT_repeat${i}_a8.txt
+#bash ./scripts/tune_mahalanobis.sh /data/csxjiang/ood_ckpt/ckpt/LT_repeat${i}_a8/epoch_100.pth \
+#    ./maha_ckpts/LT_repeat${i}_a8 /data/csxjiang/meta/train_LT_repeat${i}_a8.txt
 for dataset in ${list_dataset[*]}
 do
 bash  ./custom_test_v3_.sh Mahalanobis $dataset /data/csxjiang/ood_ckpt/ckpt/LT_repeat${i}_a8/epoch_100.pth \
-      checkpoint0919/maha_repeat${i}_a8 /data/csxjiang/meta/train_LT_repeat${i}_a8.txt 0 \
+      checkpoint0920/maha_repeat${i}_a8 /data/csxjiang/meta/train_LT_repeat${i}_a8.txt 0 \
       --mahalanobis_param_path ./maha_ckpts/LT_repeat${i}_a8/tune_mahalanobis/
 done
 done
