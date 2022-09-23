741b758c54faf5714482e2df0ac6d8ab9cec8042
jx <763565607@qq.com>
2022-09-23 16:30:34 +0800

    dice-jx commit

--- mmclassification_dev/custom_config/mobilenet3_large.py
+++ mmclassification_dev/custom_config/mobilenet3_large.py
@@ -30,7 +30,7 @@ model = dict(
     backbone=dict(type='MobileNetV3', arch='large'),
     neck=dict(type='GlobalAveragePooling'),
     head=dict(
-        type='DiceStackedLinearClsHead',
+        type='StackedLinearClsHead',
         num_classes=1000,
         in_channels=960,
         mid_channels=[1280],
@@ -38,8 +38,8 @@ model = dict(
         act_cfg=dict(type='HSwish'),
         loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
         topk=(1, 5),
-        info=info,
-        p=0.7,
+        # info=info,
+        # p=0.7,
     )))
 
 pipline =[dict(type='Collect', keys=['img', 'type'])]
