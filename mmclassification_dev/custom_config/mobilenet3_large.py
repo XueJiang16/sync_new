e7db15dccef26835de1e62f53865296fa62a64a8
jx <763565607@qq.com>
2022-09-23 16:30:07 +0800

    dice-jx commit

--- mmclassification_dev/custom_config/mobilenet3_large.py
+++ mmclassification_dev/custom_config/mobilenet3_large.py
@@ -4,7 +4,7 @@ info=os.path.expanduser('/data/csxjiang/dice_cache/imagenet_mobile_a8_feature_st
 
 method_list = ["GradNormBatch", "MSP", "Energy", "ODIN"]
 # method_list = ["GradNormBatch", "MSPCustom", "EnergyCustom", "ODINCustom"]
-method_name = method_list[2]
+method_name = method_list[1]
 model_name = 'resnet101'
 train_dataset = 'a8'
 custom_name = None
