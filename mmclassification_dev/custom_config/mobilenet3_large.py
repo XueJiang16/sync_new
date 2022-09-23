13c5cf9e678e129104ecf624d5656c6a672d7037
jx <763565607@qq.com>
2022-09-23 16:49:09 +0800

    jx commit

--- mmclassification_dev/custom_config/mobilenet3_large.py
+++ mmclassification_dev/custom_config/mobilenet3_large.py
@@ -2,8 +2,8 @@ import os
 
 info = os.path.expanduser('/data/csxjiang/dice_cache/imagenet_mobile_a8_feature_stat.pth')
 
-method_list = ["GradNormBatch", "MSP", "Energy", "ODIN"]
-# method_list = ["GradNormBatch", "MSPCustom", "EnergyCustom", "ODINCustom"]
+# method_list = ["GradNormBatch", "MSP", "Energy", "ODIN"]
+method_list = ["GradNormBatch", "MSPCustom", "EnergyCustom", "ODINCustom"]
 method_name = method_list[1]
 model_name = 'resnet101'
 train_dataset = 'a8'
