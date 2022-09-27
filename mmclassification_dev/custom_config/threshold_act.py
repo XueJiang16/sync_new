5f8efd979bf0f3094baa0f2af835562dd5145802
jx <763565607@qq.com>
2022-09-27 23:23:56 +0800

    jx commit

--- mmclassification_dev/custom_config/threshold_act.py
+++ mmclassification_dev/custom_config/threshold_act.py
@@ -1,5 +1,5 @@
-# method_name = 'ThresholdActivation'
-method_name = 'Energy'
+method_name = 'ThresholdActivation'
+# method_name = 'Energy'
 model_name = 'resnet50'
 train_dataset = 'Balance'
 custom_name = None
