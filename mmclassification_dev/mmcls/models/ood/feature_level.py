399ee8f994e263013f721eb52f5fe12bfddce9ec
jx <763565607@qq.com>
2022-09-28 00:41:15 +0800

    jx commit

--- mmclassification_dev/mmcls/models/ood/feature_level.py
+++ mmclassification_dev/mmcls/models/ood/feature_level.py
@@ -145,8 +145,8 @@ class FeatureMapSim(BaseModule):
             ood_scores = patch_sim
         if self.has_ood_detector:
             ood_scores, _ = self.ood_detector(**input)
-            patch_sim = ((1 / self.threshold) ** (self.order)) * torch.pow(patch_sim, self.order)
-            patch_sim[patch_sim > 1] = 1
+            # patch_sim = ((1 / self.threshold) ** (self.order)) * torch.pow(patch_sim, self.order)
+            # patch_sim[patch_sim > 1] = 1
             ood_scores *= patch_sim
         else:
             ood_scores = patch_sim
