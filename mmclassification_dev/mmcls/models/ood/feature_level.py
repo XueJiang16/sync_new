d34c4cc9beecbf722a65cab228fa66a46a508893
jx <763565607@qq.com>
2022-09-28 22:08:24 +0800

    jx commit

--- mmclassification_dev/mmcls/models/ood/feature_level.py
+++ mmclassification_dev/mmcls/models/ood/feature_level.py
@@ -125,18 +125,18 @@ class FeatureMapSim(BaseModule):
                 # feature_c5 = feature_c5[:,:,1:6,1:6]
                 feature_crops = feature_c5.flatten(2)
                 patch_mean = feature_crops.mean(-1).unsqueeze(-1)  # (N, C, H*W) -> (N, C)
-                patch_sim = torch.abs(feature_crops - patch_mean).mean(dim=(-1, -2))  # for ID: .mean(dim=-2)
-                # exclude 3-sigma
-                # patch_sim = torch.abs(feature_crops - patch_mean).mean(dim=-1)
-                # patch_sim_std = patch_sim.std(dim=-1)
-                # patch_sim_mean = patch_sim.mean(dim=-1)
-                # lower = patch_sim_mean-3*patch_sim_std
-                # upper = patch_sim_mean+3*patch_sim_std
-                # lower = lower.unsqueeze(1)
-                # upper = upper.unsqueeze(1)
-                # patch_sim = torch.where(patch_sim < lower, lower, patch_sim)
-                # patch_sim = torch.where(patch_sim > upper, upper, patch_sim)
-                # patch_sim = patch_sim.mean(-1)
+                # patch_sim = torch.abs(feature_crops - patch_mean).mean(dim=(-1, -2))  # for ID: .mean(dim=-2)
+                ## exclude 3-sigma
+                patch_sim = torch.abs(feature_crops - patch_mean).mean(dim=-1)
+                patch_sim_std = patch_sim.std(dim=-1)
+                patch_sim_mean = patch_sim.mean(dim=-1)
+                lower = patch_sim_mean-patch_sim_std
+                upper = patch_sim_mean+patch_sim_std
+                lower = lower.unsqueeze(1)
+                upper = upper.unsqueeze(1)
+                patch_sim = torch.where(patch_sim < lower, lower, patch_sim)
+                patch_sim = torch.where(patch_sim > upper, upper, patch_sim)
+                patch_sim = patch_sim.mean(-1)
 
             elif self.mode == 'channel_mean':
                 feature_crops = feature_c5.flatten(2)
