36ec3e78afe0cdfd9e79a2275b70757267bd8487
jx <763565607@qq.com>
2022-09-29 23:08:16 +0800

    jx multiple hyper

--- mmclassification_dev/custom_config/patchsim.py
+++ mmclassification_dev/custom_config/patchsim.py
@@ -17,7 +17,7 @@ model = dict(
     order = 1,
     mode = 'mean',
     ood_detector = dict(
-        type= method_list[1],
+        type= method_list[2],
         debug_mode=False,
         num_classes=1000,
         # temperature=1,
@@ -51,7 +51,7 @@ model = dict(
 )
 pipline =[dict(type='Collect', keys=['img', 'type'])]
 data = dict(
-    samples_per_gpu=64,
+    samples_per_gpu=256,
     workers_per_gpu=4,
     # id_data=dict(
     #     name='ImageNet',
