ecb8f71d80b09e70ba7acb39b403a3e767e9f781
jx <763565607@qq.com>
2022-10-14 23:49:21 +0800

    jx the act tune

--- mmclassification_dev/custom_config/resnet50.py
+++ mmclassification_dev/custom_config/resnet50.py
@@ -27,8 +27,8 @@ model = dict(
             out_indices=(3,),
             style='pytorch',
             random_block=[1],
-            random_block_k=[0.32],
-            random_block_location=[3],  ## 0:C2 1:C3 2:C4 3:C5
+            random_block_k=[0.3],
+            random_block_location=[2],  ## 0:C2 1:C3 2:C4 3:C5
         ),
         neck=dict(type='GlobalAveragePooling'),
         head=dict(
