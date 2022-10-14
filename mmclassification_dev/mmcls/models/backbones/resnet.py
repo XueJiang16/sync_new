c07ca9baa2145009f7082a58988b4bae4f1f18b0
jx <763565607@qq.com>
2022-10-14 23:22:57 +0800

    jx ash + featuresim

--- mmclassification_dev/mmcls/models/backbones/resnet.py
+++ mmclassification_dev/mmcls/models/backbones/resnet.py
@@ -299,8 +299,8 @@ class RandomBlock(BaseModule):
     def forward(self, x, th_act=False):
         if th_act:
             percentile_th = torch.quantile(x.flatten(1), self.k, dim=1)
-            print(percentile_th.shape)
-            assert False
+            # print(percentile_th.shape)
+            # assert False
             out = x - percentile_th
             # out = x - self.k
             out = self.non_linear(out)
