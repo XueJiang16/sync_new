ee7c039bc381f3ae4c4d2333e8aa5fa8389afffc
jx <763565607@qq.com>
2022-10-17 00:02:51 +0800

    jx the act tune

--- mmclassification_dev/mmcls/models/classifiers/image.py
+++ mmclassification_dev/mmcls/models/classifiers/image.py
@@ -160,9 +160,9 @@ class ImageClassifier(BaseClassifier):
             x_ = self.extract_feat(img, stage='backbone')[int(require_backbone_features_idx)].detach().clone()
         x = self.extract_feat(img, th_act=th_act)
 
-        x_sum = x[0].sum(dim=[1,2,3])
+        x_sum = x.sum(dim=[1,2,3])
         ratio = (x_sum - 30000) / 10000
-        x[0] = x[0] * ratio[:, None, None, None]
+        x = x * ratio[:, None, None, None]
 
 
 
