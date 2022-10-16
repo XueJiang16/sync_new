6fa50c2b4b228668580c2b267003d8df61b2a1bc
jx <763565607@qq.com>
2022-10-17 00:41:04 +0800

    jx the act tune

--- mmclassification_dev/mmcls/models/ood/feature_level.py
+++ mmclassification_dev/mmcls/models/ood/feature_level.py
@@ -162,7 +162,7 @@ class FeatureMapSim(BaseModule):
             ood_scores, _ = self.ood_detector(**input)
             # patch_sim = ((1 / self.threshold) ** (self.order)) * torch.pow(patch_sim, self.order)
             # patch_sim[patch_sim > 1] = 1
-            ood_scores = ood_scores -2
+            ood_scores = ood_scores -1
             ood_scores *= patch_sim
             ## add strategies
             # batch_ratio = (ood_scores / patch_sim).abs().cpu()
