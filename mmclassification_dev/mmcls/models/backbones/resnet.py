54cececd0a67c09f3d27b057c5b991c74f1046ae
jx <763565607@qq.com>
2022-10-16 23:27:59 +0800

    jx the act tune

--- mmclassification_dev/mmcls/models/backbones/resnet.py
+++ mmclassification_dev/mmcls/models/backbones/resnet.py
@@ -313,7 +313,7 @@ class RandomBlock(BaseModule):
             # ratio = after_sum / (before_sum + 1e-5)
             # count_ratio = after_count / before_count
             count_ratio = (before_sum - 30000) / 10000
-            count_ratio = count_ratio**2
+            count_ratio = count_ratio**1.5
             # print("Before: sum={}, count={}. After:sum={}, count={}".
             #       format(before_sum.mean(), before_count.mean(), after_sum.mean(), after_count.mean()))
             out = out * count_ratio[:, None, None, None]
