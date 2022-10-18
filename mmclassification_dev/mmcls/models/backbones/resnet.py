79213c1f0d26ba583dbcfeb36abf69d75989f552
jx <763565607@qq.com>
2022-10-18 23:44:34 +0800

    jx add noise to conv

--- mmclassification_dev/mmcls/models/backbones/resnet.py
+++ mmclassification_dev/mmcls/models/backbones/resnet.py
@@ -768,10 +768,10 @@ class ResNet(BaseBackbone):
 
     def change_weights(self):
         with torch.no_grad():
-            # noise = (torch.rand_like(self.layer3[5].conv3.weight) - 0.5) / 2.5
-            # self.layer3[5].conv3.weight += noise
-            noise = (torch.rand_like(self.layer4[0].conv1.weight) - 0.5) / 2.5
-            self.layer4[0].conv1.weight.data += noise
+            noise = (torch.rand_like(self.layer3[5].conv3.weight) - 0.5) / 2.5
+            self.layer3[5].conv3.weight += noise
+            # noise = (torch.rand_like(self.layer4[0].conv1.weight) - 0.5) / 2.5
+            # self.layer4[0].conv1.weight.data += noise
             # state_dict = self.state_dict()
             # for name, param in state_dict.items():
             #     if name == 'layer4.0.conv1.weight':
