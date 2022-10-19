9e303a2897d2472c76379b0ad4c482a8afcd4281
jx <763565607@qq.com>
2022-10-20 00:10:24 +0800

    jx own score

--- mmclassification_dev/custom_config/patchsim.py
+++ mmclassification_dev/custom_config/patchsim.py
@@ -2,7 +2,7 @@ method_list = ['MSP', 'ODIN', 'Energy', 'GradNormBatch', 'ThresholdActivation']
 method_name = 'FeatureMapSim'
 model_name = 'resnet50'
 train_dataset = 'Balance'
-custom_name = 'th_act'
+custom_name = 'triple'
 if custom_name is not None:
     readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
 else:
@@ -17,7 +17,7 @@ model = dict(
     order = 1,
     mode = 'mean',
     ood_detector = dict(
-        type= method_list[-1],
+        type= method_list[2],
         debug_mode=False,
         num_classes=1000,
         # temperature=1,
