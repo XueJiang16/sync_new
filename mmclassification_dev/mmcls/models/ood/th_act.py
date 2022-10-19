aa5f444e8baa9ea4074d7c6a4582bd9e32981c70
jx <763565607@qq.com>
2022-10-19 23:12:26 +0800

    jx add noise to conv

--- mmclassification_dev/mmcls/models/ood/th_act.py
+++ mmclassification_dev/mmcls/models/ood/th_act.py
@@ -29,23 +29,23 @@ class ThresholdActivation(BaseModule):
 
         with torch.no_grad():
             #  the threshold act diff
-            # confs_orig = self.classifier(return_loss=False, softmax=False, post_process=False, **input)
+            confs_orig = self.classifier(return_loss=False, softmax=False, post_process=False, **input)
             # confs_orig = self.criterion(confs_orig)
             # # confs_orig, pred_idx_orig = torch.max(confs_orig, dim=-1)
             # # pred_idx_orig = pred_idx_orig.unsqueeze(1)
-            # confs_th_act = self.classifier(return_loss=False, softmax=False, post_process=False, th_act=True, **input)
+            confs_th_act = self.classifier(return_loss=False, softmax=False, post_process=False, th_act=True, **input)
             # # confs_th_act = self.criterion(confs_th_act)
             # # confs_th_act = torch.gather(confs_th_act, 1, pred_idx_orig).squeeze(1)
-            # ood_scores = -torch.abs(confs_orig-confs_th_act).sum(1)
-            _, features_orig = self.classifier(return_loss=False, softmax=False, post_process=False,
-                                               th_act=False,require_features=True, **input)
-            _, features_th_act = self.classifier(return_loss=False, softmax=False, post_process=False,
-                                                 th_act=True, require_features=True,**input)
+            ood_scores = -torch.abs(confs_orig-confs_th_act).mean(1)
+            # _, features_orig = self.classifier(return_loss=False, softmax=False, post_process=False,
+            #                                    th_act=False,require_features=True, **input)
+            # _, features_th_act = self.classifier(return_loss=False, softmax=False, post_process=False,
+            #                                      th_act=True, require_features=True,**input)
             # ood_scores = - torch.linalg.norm(features_orig-features_th_act, ord=2, dim=-1)
             #  cosine sim
             # sim = features_orig * features_th_act
             # ood_scores = sim.sum(1) / (torch.norm(features_orig, dim=1) * torch.norm(features_th_act, dim=1))
 
             #KL
-            ood_scores = -torch.nn.functional.kl_div(features_orig, features_th_act, reduction='none').mean(1)
+            # ood_scores = -torch.nn.functional.kl_div(features_orig, features_th_act, reduction='none').mean(1)
             return ood_scores, type
