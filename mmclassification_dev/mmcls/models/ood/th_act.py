269f3e33137a245f9cfca33dbe7b871f29af45e3
jx <763565607@qq.com>
2022-10-19 23:24:53 +0800

    jx own score

--- mmclassification_dev/mmcls/models/ood/th_act.py
+++ mmclassification_dev/mmcls/models/ood/th_act.py
@@ -30,14 +30,14 @@ class ThresholdActivation(BaseModule):
         with torch.no_grad():
             #  the threshold act diff
             confs_orig = self.classifier(return_loss=False, softmax=False, post_process=False, th_act=False,**input)
-            # confs_orig = self.criterion(confs_orig)
+            confs_orig = self.criterion(confs_orig)
             # # confs_orig, pred_idx_orig = torch.max(confs_orig, dim=-1)
             # # pred_idx_orig = pred_idx_orig.unsqueeze(1)
             confs_th_act = self.classifier(return_loss=False, softmax=False, post_process=False, th_act=True, **input)
-            # confs_th_act = self.criterion(confs_th_act)
+            confs_th_act = self.criterion(confs_th_act)
             # # confs_th_act = torch.gather(confs_th_act, 1, pred_idx_orig).squeeze(1)
             # ood_scores = -torch.abs(confs_orig-confs_th_act).sum(1)
-            ood_scores = -torch.nn.functional.kl_div(confs_orig, confs_th_act, reduction='none').sum(1)
+            ood_scores = torch.nn.functional.kl_div(confs_orig, confs_th_act, reduction='none').sum(1)
             # sim = confs_orig * confs_th_act
             # ood_scores = sim.sum(1) / (torch.norm(confs_orig, dim=1) * torch.norm(confs_th_act, dim=1))
             # _, features_orig = self.classifier(return_loss=False, softmax=False, post_process=False,
--- mmclassification_dev/run.sh
+++ mmclassification_dev/run.sh
@@ -8,11 +8,11 @@
 #bash ./tools/ood_dist_test.sh custom_config/resnet50.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/resnet50_official.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/meanstd.py foo 2
-# bash ./tools/ood_dist_test.sh custom_config/patchsim.py foo 2
+ bash ./tools/ood_dist_test.sh custom_config/patchsim.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/inat_ib/resnet101.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/aug_contrast.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/cifar10.py foo 2
-bash ./tools/ood_dist_test.sh custom_config/threshold_act.py foo 2
+#bash ./tools/ood_dist_test.sh custom_config/threshold_act.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/vis_features.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/multi_modal/clip.py foo 2 
 #bash ./tools/dist_precompute.sh custom_config/dice_precompute.py /data/csxjiang/ood_ckpt/ckpt/LT_repeat3_a8/epoch_100.pth 2 --metrics accuracy
