c91a75de97621901e6fe6d2b69af63c72f8c5f62
jx <763565607@qq.com>
2022-10-19 22:54:22 +0800

    jx add noise to conv

--- mmclassification_dev/mmcls/models/ood/feature_level.py
+++ mmclassification_dev/mmcls/models/ood/feature_level.py
@@ -163,6 +163,9 @@ class FeatureMapSim(BaseModule):
             # patch_sim = ((1 / self.threshold) ** (self.order)) * torch.pow(patch_sim, self.order)
             # patch_sim[patch_sim > 1] = 1
             ood_scores = ood_scores
+            print("mean:",ood_scores.mean())
+            print("std:",ood_scores.std())
+            exit()
             ood_scores *= patch_sim
             ## add strategies
             # batch_ratio = (ood_scores / patch_sim).abs().cpu()
