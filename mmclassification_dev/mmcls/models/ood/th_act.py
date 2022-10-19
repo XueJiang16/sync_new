726e0df157edf4e383d62e0144350ccae8d41510
jx <763565607@qq.com>
2022-10-19 23:54:35 +0800

    jx own score

--- mmclassification_dev/mmcls/models/ood/th_act.py
+++ mmclassification_dev/mmcls/models/ood/th_act.py
@@ -39,7 +39,7 @@ class ThresholdActivation(BaseModule):
             # # confs_th_act = torch.gather(confs_th_act, 1, pred_idx_orig).squeeze(1)
             # ood_scores = -torch.abs(confs_orig-confs_th_act).sum(1)
             sim = torch.nn.functional.kl_div(confs_orig, confs_th_act, reduction='none').sum(1) + 7
-            ood_scores = prediction * sim
+            ood_scores = prediction
             # sim = confs_orig * confs_th_act
             # ood_scores = sim.sum(1) / (torch.norm(confs_orig, dim=1) * torch.norm(confs_th_act, dim=1))
             # _, features_orig = self.classifier(return_loss=False, softmax=False, post_process=False,
