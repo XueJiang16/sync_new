4a16d916d226274eadf5db92230afa9cf6fe6201
jx <763565607@qq.com>
2022-10-25 00:18:16 +0800

    jx aug add

--- mmclassification_dev/mmcls/datasets/ood_dataset.py
+++ mmclassification_dev/mmcls/datasets/ood_dataset.py
@@ -66,8 +66,8 @@ class OODBaseDataset(Dataset):
             seq = iaa.Sequential([
                 # iaa.imgcorruptlike.Fog(severity=5)
                 # iaa.GammaContrast((0.8, 1.2)),
-                # iaa.LogContrast(gain=(0.8, 1.2)),
-                iaa.MultiplyAndAddToBrightness(mul=(0.5, 0.5), add=(0, 0)),
+                iaa.LogContrast(gain=(0.5,0.5)),
+                # iaa.MultiplyAndAddToBrightness(mul=(0.5, 0.5), add=(0, 0)),
                 # iaa.AddToBrightness((-10, 10))
             ])
             sample = np.array(sample).astype('uint8')
