5f698a0510b4769339d59e8860186092b5413449
jx <763565607@qq.com>
2022-10-24 23:52:43 +0800

    jx aug add

--- mmclassification_dev/custom_config/resnet50.py
+++ mmclassification_dev/custom_config/resnet50.py
@@ -1,5 +1,5 @@
 method_list = ['MSP', 'ODIN', 'Energy', 'GradNormBatch']
-method_name = method_list[0]
+method_name = method_list[1]
 model_name = 'resnet50'
 train_dataset = 'Balance_with_fog_aug'
 custom_name = None
