de74139e4833c83d479c4477c4a91330c4112737
jx <763565607@qq.com>
2022-10-31 23:02:51 +0800

    jx top k pooling

--- mmclassification_dev/custom_config/patchsim.py
+++ mmclassification_dev/custom_config/patchsim.py
@@ -41,7 +41,7 @@ model = dict(
             ),
             # neck=dict(type='GlobalAveragePooling'),
             neck=dict(type='TopKAveragePooling',
-                      k=0.5),
+                      k=0.3),
             head=dict(
                 type='LinearClsHead',
                 num_classes=1000,
