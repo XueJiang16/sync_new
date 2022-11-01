cde00f569afa18c578b1ea1fd2c492e622cfcb58
jx <763565607@qq.com>
2022-11-01 22:45:19 +0800

    jx top k pooling

--- mmclassification_dev/custom_config/validation/resnet50.py
+++ mmclassification_dev/custom_config/validation/resnet50.py
@@ -28,7 +28,7 @@ if custom_name is not None:
     readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
 else:
     readable_name ='{}_{}_{}'.format(method_name, model_name, train_dataset)
-quick_test = False
+quick_test = True
 noise_engine = None
 
 model = dict(
