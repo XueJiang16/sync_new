5d9aa720b33f573a620e30a0211fbabc48cdfa7d
jx <763565607@qq.com>
2022-11-01 23:09:31 +0800

    jx top k pooling

--- mmclassification_dev/custom_config/validation/resnet50.py
+++ mmclassification_dev/custom_config/validation/resnet50.py
@@ -90,7 +90,7 @@ model = dict(
             ),
             # neck=dict(type='GlobalAveragePooling'),
             neck=dict(type='TopKAveragePooling',
-                      k=0.6),
+                      k=0.5),
             head=dict(
                 type='LinearClsHead',
                 num_classes=train_num_class,
