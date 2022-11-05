e338591af70009991837e07b7ce4555bc8ac33d8
jx <763565607@qq.com>
2022-11-05 17:01:26 +0800

    jx val

--- mmclassification_dev/custom_config/resnet101_official.py
+++ mmclassification_dev/custom_config/resnet101_official.py
@@ -1,4 +1,4 @@
-method_name = 'MSP'
+method_name = 'Energy'
 model_name = 'resnet101'
 train_dataset = 'Balance'
 custom_name = "Official"
--- mmclassification_dev/run.sh
+++ mmclassification_dev/run.sh
@@ -1,5 +1,6 @@
 #gits
-#bash ./tools/ood_dist_test.sh custom_config/resnet101.py foo 2 
+#bash ./tools/ood_dist_test.sh custom_config/resnet101.py foo 2
+bash ./tools/ood_dist_test.sh custom_config/resnet101_official.py foo 2
 #python ./tools/ood_test.py custom_config/resnet50.py foo 
 #bash ./tools/ood_dist_test.sh custom_config/resnet101_nest_cfg.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/Noise/resnet50_uniform.py foo 2
@@ -8,7 +9,7 @@
 # bash ./tools/ood_dist_test.sh custom_config/resnet50.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/resnet50_official.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/meanstd.py foo 2
-bash ./tools/ood_dist_test.sh custom_config/patchsim.py foo 2
+#bash ./tools/ood_dist_test.sh custom_config/patchsim.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/inat_ib/resnet101.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/aug_contrast.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/cifar10.py foo 2
