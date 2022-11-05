a3398fac31ecd06770e69fe8bb616c69f10e51aa
jx <763565607@qq.com>
2022-11-05 17:17:16 +0800

    jx rebuttal

--- mmclassification_dev/custom_config/resnet101_official.py
+++ mmclassification_dev/custom_config/resnet101_official.py
@@ -1,4 +1,4 @@
-method_name = 'Cosine'
+method_name = 'ODIN'
 model_name = 'resnet101'
 train_dataset = 'Balance'
 custom_name = "Official"
@@ -34,7 +34,7 @@ pipline =[
     dict(type='Collect', keys=['img', 'type'])
 ]
 data = dict(
-    samples_per_gpu=256,
+    samples_per_gpu=32,
     workers_per_gpu=4,
     id_data=dict(
         name='ImageNet',
