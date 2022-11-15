1dc66e7042eea6ca0f5339c1713d34ef1d479801
jx <763565607@qq.com>
2022-11-16 00:17:33 +0800

    jx rebuttal

--- mmclassification_dev/mmcls/datasets/ood_dataset.py
+++ mmclassification_dev/mmcls/datasets/ood_dataset.py
@@ -28,7 +28,8 @@ class OODBaseDataset(Dataset):
         self.data_prefix = None
         self.name = name
         self.transform = tv.transforms.Compose([
-            tv.transforms.Resize(248),
+            # tv.transforms.Resize(256),
+            tv.transforms.Resize(248, interpolation=tv.InterpolationMode.BICUBIC)
             tv.transforms.CenterCrop(224),
             # tv.transforms.Resize((480, 480)),
             tv.transforms.ToTensor(),
