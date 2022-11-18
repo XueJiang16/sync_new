2cd2640ec632dff1a25ccc465b784be6a25ac165
jx <763565607@qq.com>
2022-11-18 21:37:49 +0800

    jx rebuttal

--- mmclassification_dev/custom_config/vit_lt.py
+++ mmclassification_dev/custom_config/vit_lt.py
@@ -1,8 +1,8 @@
 import os
 
-# method_list = ["GradNormBatch", "MSP", "Energy", "ODIN"]
-method_list = ["GradNormBatch", "MSPCustom", "EnergyCustom", "ODINCustom"]
-method_name = method_list[1]
+method_list = ["GradNormBatch", "MSP", "Energy", "ODIN"]
+# method_list = ["GradNormBatch", "MSPCustom", "EnergyCustom", "ODINCustom"]
+method_name = method_list[3]
 model_name = 'resnet101'
 train_dataset = 'a8'
 # info=os.path.expanduser('/data/csxjiang/dice_cache/imagenet_res101_{}_feature_stat.pth'.format(train_dataset))
