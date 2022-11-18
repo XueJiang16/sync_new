e85d166478955933389b4c8fd4dbbf8571c0dc27
jx <763565607@qq.com>
2022-11-18 22:00:28 +0800

    jx rebuttal

--- mmclassification_dev/mmcls/datasets/ood_dataset.py
+++ mmclassification_dev/mmcls/datasets/ood_dataset.py
@@ -29,14 +29,14 @@ class OODBaseDataset(Dataset):
         self.name = name
         self.transform = tv.transforms.Compose([
             tv.transforms.Resize(256),
-            # tv.transforms.Resize(248, interpolation=tv.transforms.InterpolationMode.BICUBIC),
+            tv.transforms.Resize(248, interpolation=tv.transforms.InterpolationMode.BICUBIC),
             tv.transforms.CenterCrop(224),
             # tv.transforms.Resize((480, 480)),
             tv.transforms.ToTensor(),
-            tv.transforms.Normalize([123.675/255, 116.28/255, 103.53/255],
-                                    [58.395/255, 57.12/255, 57.375/255]),
-            # tv.transforms.Normalize([0.5, 0.5, 0.5],
-            #                         [0.5, 0.5, 0.5]),
+            # tv.transforms.Normalize([123.675/255, 116.28/255, 103.53/255],
+            #                         [58.395/255, 57.12/255, 57.375/255]),
+            tv.transforms.Normalize([0.5, 0.5, 0.5],
+                                    [0.5, 0.5, 0.5]),
         ])
         self.noise_engine = noise_engine
         self.len_limit = len_limit
