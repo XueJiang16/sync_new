5e8eb9726f5e6958989ef74570887d1ef686dab9
jx <763565607@qq.com>
2022-11-18 21:47:28 +0800

    jx rebuttal

--- mmclassification_dev/mmcls/models/classifiers/vit_lt.py
+++ mmclassification_dev/mmcls/models/classifiers/vit_lt.py
@@ -33,6 +33,8 @@ class VitClassifier(BaseClassifier):
         load_checkpoint(self.model, checkpoint)
         self.model = self.model.to(self.device)
         self.model.eval()
+        print(self.model)
+        assert False
 
     def extract_feat(self, img, stage='neck'):
         # TODO
