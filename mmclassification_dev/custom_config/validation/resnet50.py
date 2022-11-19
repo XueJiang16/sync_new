426ce37c35aa576559ff9325e6dc9348be640a94
jx <763565607@qq.com>
2022-11-19 23:33:42 +0800

    jx merge3

--- mmclassification_dev/custom_config/validation/resnet50.py
+++ mmclassification_dev/custom_config/validation/resnet50.py
@@ -66,8 +66,8 @@ model = dict(
     img_size=224,
     threshold=0.4,
     order=1,
-    # mode='mean',
-    mode=None,
+    mode='mean',
+    # mode=None,
     ood_detector=dict(
         type=method_list[2],
         debug_mode=False,
@@ -83,13 +83,13 @@ model = dict(
                 num_stages=4,
                 out_indices=(3,),
                 style='pytorch',
-                # random_block=[1],
-                # random_block_k=[0.01],
-                # random_block_location=[2],  # 0:C2 1:C3 2:C4 3:C5
+                random_block=[1],
+                random_block_k=[0.01],
+                random_block_location=[2],  # 0:C2 1:C3 2:C4 3:C5
             ),
-            neck=dict(type='GlobalAveragePooling'),
-            # neck=dict(type='TopKAveragePooling',
-            #           k=0.6),
+            # neck=dict(type='GlobalAveragePooling'),
+            neck=dict(type='TopKAveragePooling',
+                      k=0.6),
             head=dict(
                 type='LinearClsHead',
                 num_classes=train_num_class,
