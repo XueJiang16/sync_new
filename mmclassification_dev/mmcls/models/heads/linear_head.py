1f9354125a6c57cdfaf3a417c2aa98d1eacabba7
jx <763565607@qq.com>
2022-11-19 23:30:22 +0800

    jx merge3

--- mmclassification_dev/mmcls/models/heads/linear_head.py
+++ mmclassification_dev/mmcls/models/heads/linear_head.py
@@ -44,7 +44,7 @@ class LinearClsHead(ClsHead):
             x = x[-1]
         return x
 
-    def apply_ash(self, x, percentile=80):
+    def apply_ash(self, x, percentile=70):
 
         # Ash-S
         b, c= x.shape
