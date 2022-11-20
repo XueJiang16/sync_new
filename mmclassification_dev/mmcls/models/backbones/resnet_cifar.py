e6034362eeb85d96905ed2a999b230feac1e2121
jx <763565607@qq.com>
2022-11-20 22:41:55 +0800

    jx merge3

--- mmclassification_dev/mmcls/models/backbones/resnet_cifar.py
+++ mmclassification_dev/mmcls/models/backbones/resnet_cifar.py
@@ -68,7 +68,7 @@ class ResNet_CIFAR(ResNet):
         self.add_module(self.norm1_name, norm1)
         self.relu = nn.ReLU(inplace=True)
 
-    def forward(self, x):
+    def forward(self, x, the_act=False):
         x = self.conv1(x)
         x = self.norm1(x)
         x = self.relu(x)
