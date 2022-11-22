509c3d8d5c7d5c18e1eb6bc8463f47856e7aee4f
jx <763565607@qq.com>
2022-11-22 23:41:33 +0800

    jx merge3

--- mmclassification_dev/mmcls/models/backbones/resnet.py
+++ mmclassification_dev/mmcls/models/backbones/resnet.py
@@ -314,10 +314,10 @@ class RandomBlock(BaseModule):
         # out = topk_v.mean(dim=-1)
         mean_gap = x_gap.mean(dim=-1)
         mean_kap = out.mean(dim=-1)
-        max_gap = x_gap.max(dim=-1)
-        min_gap = x_gap.min(dim=-1)
-        max_kap = out.max(dim=-1)
-        min_kap = out.min(dim=-1)
+        max_gap = x_gap.max(dim=-1)[0]
+        min_gap = x_gap.min(dim=-1)[0]
+        max_kap = out.max(dim=-1)[0]
+        min_kap = out.min(dim=-1)[0]
         # out = out * (mean_gap / mean_kap).unsqueeze(-1)
         out = (out - min_kap) * ((max_gap - min_gap) / (max_kap - min_kap)).unsqueeze(dim=-1) + min_gap
         return out
