1ce238d049518ce8970376f845c7516000b9a234
jx <763565607@qq.com>
2022-11-23 00:05:51 +0800

    jx kap

--- mmclassification_dev/custom_config/patchsim.py
+++ mmclassification_dev/custom_config/patchsim.py
@@ -7,7 +7,7 @@ if custom_name is not None:
     readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
 else:
     readable_name ='{}_{}_{}'.format(method_name, model_name, train_dataset)
-quick_test = True
+quick_test = False
 noise_engine = None
 k_c5 = 0.65
 model = dict(
