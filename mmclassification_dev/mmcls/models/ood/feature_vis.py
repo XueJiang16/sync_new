727a271003c272f1cf9e789b9a85cc32f973678b
jx <763565607@qq.com>
2022-12-03 23:03:54 +0800

    jx auto id acc

--- mmclassification_dev/mmcls/models/ood/feature_vis.py
+++ mmclassification_dev/mmcls/models/ood/feature_vis.py
@@ -69,9 +69,12 @@ class FeatureVis(BaseModule):
             # C4_features[C4_features<k] = 0
             # C4_features[C4_features>=k] = 1
             C4_features_norm = self.norm(C4_features)
-
-            C4_features_larger = self.norm(torch.clamp(C4_features, min=k))
-            C4_features_lower = self.norm(torch.clamp(C4_features, max=k))
+            C4_features_larger = C4_features.clone()
+            C4_features_larger[C4_features_larger < k] = 0
+            C4_features_larger = self.norm(C4_features_larger)
+            C4_features_lower = C4_features.clone()
+            C4_features_lower[C4_features_lower > k] = 0
+            C4_features_lower = self.norm()
 
             # C4_features_larger = C4_features_larger - k
 
