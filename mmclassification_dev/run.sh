b8d6b86f3ec8e1d0fc998c7f2116c691d47ac35b
jx <763565607@qq.com>
2022-12-03 23:43:08 +0800

    jx auto id acc

--- mmclassification_dev/run.sh
+++ mmclassification_dev/run.sh
@@ -26,8 +26,8 @@
 #bash ./tools/ood_dist_test.sh custom_config/cifar_benchmark/resnet50.py foo 1
 
 ## google doc
-#bash ./tools/ood_dist_test_auto.sh custom_config/patchsim.py foo 2
+bash ./tools/ood_dist_test_auto.sh custom_config/patchsim.py foo 2
 
 ## id acc
 #bash ./tools/dist_test.sh custom_config/id_acc/resnet50_topk.py /data/csxjiang/ood_ckpt/pytorch_official/resnet50_custom.pth 2 --metrics accuracy
-bash ./tools/dist_test_auto.sh custom_config/id_acc/resnet50_topk.py /data/csxjiang/ood_ckpt/pytorch_official/resnet50_custom.pth 2 --metrics accuracy
+#bash ./tools/dist_test_auto.sh custom_config/id_acc/resnet50_topk.py /data/csxjiang/ood_ckpt/pytorch_official/resnet50_custom.pth 2 --metrics accuracy
