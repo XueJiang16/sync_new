956120b65eb0e0aaa6f8074708b34472017aca1e
jx <763565607@qq.com>
2022-12-03 21:06:31 +0800

    jx auto id acc

--- mmclassification_dev/tools/test_auto.py
+++ mmclassification_dev/tools/test_auto.py
@@ -144,7 +144,8 @@ def main(args, task_cfg, is_init=False, gs=None):
         except:
             v = "'{}'".format(v)
         exec("{} = {}".format(k, v))
-
+    print(cfg.model.neck.k)
+    assert False
 
 
 
