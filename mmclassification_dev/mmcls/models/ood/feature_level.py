9c2d566d7e4ba5f0bc8d338c08405ac78825ccc4
jx <763565607@qq.com>
2022-12-04 23:26:46 +0800

    jx auto id acc

--- mmclassification_dev/mmcls/models/ood/feature_level.py
+++ mmclassification_dev/mmcls/models/ood/feature_level.py
@@ -206,8 +206,8 @@ class FeatureMapSim(BaseModule):
             # with torch.no_grad():
 
             # ood_scores = ood_scores * kl_sim * patch_sim
-            # ood_scores += 1.4
-            ood_scores *= (patch_sim + self.fuse_const)
+            ood_scores += self.fuse_const
+            ood_scores *= patch_sim
             # print("mean:", ood_scores.mean())
             # print("std:", ood_scores.std())
             # exit()
