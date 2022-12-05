d23301d285f5eb26da00cf58093190ca43964a6a
jx <763565607@qq.com>
2022-12-05 23:28:48 +0800

    jx auto id acc

--- mmclassification_dev/mmcls/models/ood/dynamic_th_act.py
+++ mmclassification_dev/mmcls/models/ood/dynamic_th_act.py
@@ -14,7 +14,7 @@ def no_ood_detector(**kwargs):
 
 @OOD.register_module()
 class DynamicThresholdActivation(BaseModule):
-    def __init__(self, k=1, ood_detector=None,**kwargs):
+    def __init__(self, a, b, k=1, ood_detector=None,**kwargs):
         super(DynamicThresholdActivation, self).__init__()
         self.local_rank = os.environ['LOCAL_RANK']
         self.has_ood_detector = True if ood_detector else False
@@ -23,6 +23,8 @@ class DynamicThresholdActivation(BaseModule):
         else:
             self.ood_detector = no_ood_detector
         self.k = k
+        self.a = a
+        self.b = b
 
     def forward(self, **input):
         if "type" in input:
@@ -34,7 +36,7 @@ class DynamicThresholdActivation(BaseModule):
                                                     require_backbone_features=False, th_act=False, **input)
             pred_conf, pred_class = torch.max(outputs,dim=-1)
             # update_k = self.k
-            update_k = 0.75*self.k*pred_conf+0.75*self.k
+            update_k = (self.b-self.a)*self.k*pred_conf+self.a*self.k
             new_outputs = self.ood_detector.classifier(return_loss=False, softmax=True, post_process=False,
                                                     require_backbone_features=False, th_act=update_k, **input)
             # ood_scores = self.ood_detector(**input)
