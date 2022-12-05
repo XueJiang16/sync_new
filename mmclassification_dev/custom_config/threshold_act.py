fa800be2f291850b4e75eb4aac26767575f39d93
jx <763565607@qq.com>
2022-12-05 22:17:24 +0800

    jx auto id acc

--- mmclassification_dev/custom_config/threshold_act.py
+++ mmclassification_dev/custom_config/threshold_act.py
@@ -1,5 +1,5 @@
-method_name = 'ThresholdActivation'
-# method_name = 'Energy'
+method_list = ['MSP', 'ODIN', 'Energy', 'GradNormBatch', 'ThresholdActivation']
+method_name = method_list[3]
 model_name = 'resnet50'
 train_dataset = 'Balance'
 custom_name = None
