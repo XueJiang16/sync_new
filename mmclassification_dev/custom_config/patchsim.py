dc7004391675dae375bc389bd5b14a298bc97ccb
jx <763565607@qq.com>
2022-12-05 22:14:39 +0800

    jx auto id acc

--- mmclassification_dev/custom_config/patchsim.py
+++ mmclassification_dev/custom_config/patchsim.py
@@ -17,7 +17,7 @@ model = dict(
     threshold=0.4,
     order=1,
     k=k_c5,
-    mode=None,
+    mode='kap',
     fuse_const=0,
     ood_detector=dict(
         type=method_list[2],
@@ -38,9 +38,9 @@ model = dict(
                 random_block_k=[0.2],
                 random_block_location=[2],  # 0:C2 1:C3 2:C4 3:C5
             ),
-            neck=dict(type='GlobalAveragePooling'),
-            # neck=dict(type='TopKAveragePooling',
-            #           k=k_c5),
+            # neck=dict(type='GlobalAveragePooling'),
+            neck=dict(type='TopKAveragePooling',
+                      k=k_c5),
             head=dict(
                 type='LinearClsHead',
                 num_classes=1000,
