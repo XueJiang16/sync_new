2c07f4d287ee0665c33241f7b9b9ad1b86fa512e
1@whughw.com <ghw970403>
2022-12-10 11:07:46 +0800

    ghw commit: dump softmax score for lt-ood detection

--- mmclassification_dev/tools/softmax_compare.py
+++ mmclassification_dev/tools/softmax_compare.py
@@ -7,7 +7,7 @@ target_path = "/home/csxjiang/jx/sync/mmclassification_dev/softmax_dump"
 ID_dir = "ID"
 OOD_dir = ["iNaturalist", "Places", "SUN", "Textures"]
 ID_samples = []
-for sample in tqdm.tqdm(os.listdir(ID_dir)):
+for sample in tqdm.tqdm(os.listdir(os.path.join(target_path, ID_dir))):
     f = open(os.path.join(target_path, ID_dir, sample), "r")
     with f:
         ID_samples.append(np.array(eval(f.readline().strip())))
@@ -15,7 +15,7 @@ for sample in tqdm.tqdm(os.listdir(ID_dir)):
     assert False
 OOD_samples = []
 for OOD_subdir in OOD_dir:
-    for sample in tqdm.tqdm(os.listdir(OOD_subdir)):
+    for sample in tqdm.tqdm(os.listdir(os.path.join(target_path, OOD_subdir))):
         f = open(os.path.join(target_path, OOD_subdir, sample), "r")
         with f:
             OOD_samples.append(np.array(eval(f.readline().strip())))
