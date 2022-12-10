78de68fee05bf8d86556561f80519833445d0916
1@whughw.com <ghw970403>
2022-12-10 11:21:18 +0800

    ghw commit: dump softmax score for lt-ood detection

--- mmclassification_dev/tools/softmax_compare.py
+++ mmclassification_dev/tools/softmax_compare.py
@@ -4,18 +4,38 @@ from sklearn.neighbors import KDTree as TUTUTree
 import tqdm
 
 target_path = "/home/csxjiang/jx/sync/mmclassification_dev/softmax_dump"
+matched = {}
 ID_dir = "ID"
 OOD_dir = ["iNaturalist", "Places", "SUN", "Textures"]
-ID_samples = []
+full_samples = []
+full_names = []
+full_types = []
 for sample in tqdm.tqdm(os.listdir(os.path.join(target_path, ID_dir))):
     f = open(os.path.join(target_path, ID_dir, sample), "r")
     with f:
-        ID_samples.append(np.array(eval(f.readline().strip())))
-    print(ID_samples[0])
-    assert False
+        softmax_score = np.array(eval(f.readline().strip()))
+        full_samples.append(softmax_score)
+        full_names.append(os.path.splitext(sample)[0])
+        full_types.append("ID")
 OOD_samples = []
 for OOD_subdir in OOD_dir:
     for sample in tqdm.tqdm(os.listdir(os.path.join(target_path, OOD_subdir))):
         f = open(os.path.join(target_path, OOD_subdir, sample), "r")
         with f:
-            OOD_samples.append(np.array(eval(f.readline().strip())))
+            softmax_score = np.array(eval(f.readline().strip()))
+            OOD_samples.append(os.path.splitext(sample)[0])
+            full_samples.append(softmax_score)
+            full_types.append("OOD")
+
+tutu = TUTUTree(full_samples, metric="l1")
+eps = 1e-3
+
+for sample, name, type in zip(full_samples, full_names, full_types):
+    if name in matched:
+        continue
+    dist, ind = tutu.query(sample, k=1)
+    dist = dist[0]
+    ind = ind[0]
+    if dist < eps:
+        print("({}){} <==> ({}){}, dist={}.".format(type, name, full_types[ind], full_names[ind], dist))
+        matched[name] = None
