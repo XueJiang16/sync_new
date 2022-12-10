fe04040a63cd40ebfe1e548104ed3095c75ec7e1
jx <763565607@qq.com>
2022-12-10 17:29:45 +0800

    jx softmax_compare

--- mmclassification_dev/custom_config/resnet101.py
+++ mmclassification_dev/custom_config/resnet101.py
@@ -2,7 +2,7 @@ import os
 
 method_list = ["GradNormBatch", "MSP", "Energy", "ODIN"]
 # method_list = ["GradNormBatch", "MSPCustom", "EnergyCustom", "ODINCustom"]
-method_name = method_list[-1]
+method_name = method_list[-2]
 model_name = 'resnet101'
 train_dataset = 'a8'
 # info=os.path.expanduser('/data/csxjiang/dice_cache/imagenet_res101_{}_feature_stat.pth'.format(train_dataset))
