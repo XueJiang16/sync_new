bb2d646d922e1463b0f14d86bfc7d5de4df2b75d
1@whughw.com <ghw970403>
2022-12-10 11:27:18 +0800

    ghw commit: dump softmax score for lt-ood detection

--- mmclassification_dev/tools/softmax_compare.py
+++ mmclassification_dev/tools/softmax_compare.py
@@ -35,8 +35,8 @@ for sample, name, type in zip(full_samples, full_names, full_types):
     if name in matched:
         continue
     dist, ind = tutu.query([sample], k=2)
-    dist = dist[1]
-    ind = ind[1]
+    dist = dist[0][1]
+    ind = ind[0][1]
     if dist < eps:
         print("({}){} <==> ({}){}, dist={}.".format(type, name, full_types[ind], full_names[ind], dist))
         matched[name] = None
