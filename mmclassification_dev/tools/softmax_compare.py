54f9e7f789642ebd19f86ec8ecc7268357943e38
1@whughw.com <ghw970403>
2022-12-10 11:33:54 +0800

    ghw commit: dump softmax score for lt-ood detection

--- mmclassification_dev/tools/softmax_compare.py
+++ mmclassification_dev/tools/softmax_compare.py
@@ -30,13 +30,14 @@ for OOD_subdir in OOD_dir:
 
 tutu = TUTUTree(full_samples, metric="l1")
 eps = 1e-3
-
-for sample, name, type in zip(full_samples, full_names, full_types):
-    if name in matched:
-        continue
-    dist, ind = tutu.query([sample], k=2)
-    dist = dist[0][1]
-    ind = ind[0][1]
-    if dist < eps:
-        print("({}){} <==> ({}){}, dist={}.".format(type, name, full_types[ind], full_names[ind], dist))
-        matched[name] = None
+with open("sim_samples.txt", mode="w") as f:
+    for sample, name, type in tqdm.tqdm(zip(full_samples, full_names, full_types)):
+        if name in matched:
+            continue
+        dist, ind = tutu.query([sample], k=2)
+        dist = dist[0][1]
+        ind = ind[0][1]
+        if dist < eps:
+            print("({}){} <==> ({}){}, dist={}.".format(type, name, full_types[ind], full_names[ind], dist))
+            f.write("{},{},{},{},{}\n".format(type, name, full_types[ind], full_names[ind], dist))
+            matched[name] = None
