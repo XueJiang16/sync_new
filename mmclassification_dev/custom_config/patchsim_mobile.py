17550647fe4b9f70f498414f0e624e609ac055a0
jx <763565607@qq.com>
2022-12-13 22:44:20 +0800

    jx mobilenet

--- mmclassification_dev/custom_config/patchsim_mobile.py
+++ mmclassification_dev/custom_config/patchsim_mobile.py
@@ -8,7 +8,7 @@ if custom_name is not None:
     readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
 else:
     readable_name ='{}_{}_{}'.format(method_name, model_name, train_dataset)
-quick_test = False
+quick_test = True
 noise_engine = None
 k_c5 = 0.65
 model = dict(
