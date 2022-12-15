475d4b2758d87df57bff2ccbcd2e591a3528813f
jx <763565607@qq.com>
2022-12-15 23:18:21 +0800

    jx resnet act

--- mmclassification_dev/custom_config/id_acc/resnet50_topk.py
+++ mmclassification_dev/custom_config/id_acc/resnet50_topk.py
@@ -2,7 +2,7 @@ model = dict(
     type='ImageClassifier',
     backbone=dict(
         type='ResNet',
-        depth=34,
+        depth=101,
         num_stages=4,
         out_indices=(3,),
         style='pytorch',
@@ -16,7 +16,7 @@ model = dict(
     head=dict(
         type='LinearClsHead',
         num_classes=1000,
-        in_channels=512,
+        in_channels=2048,
         loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
         topk=(1, 5)))
 
--- mmclassification_dev/run.sh
+++ mmclassification_dev/run.sh
@@ -28,10 +28,11 @@
 ## google doc
 #bash ./tools/ood_dist_test_auto.sh custom_config/patchsim.py foo 2 --tab_name auto3
 #bash ./tools/ood_dist_test_auto.sh custom_config/patchsim_mobile.py foo 2 --tab_name mobile
-bash ./tools/ood_dist_test_auto.sh custom_config/resnet50.py foo 2 --tab_name res101
+#bash ./tools/ood_dist_test_auto.sh custom_config/resnet50.py foo 2 --tab_name res101
 #bash ./tools/ood_dist_test_auto.sh custom_config/threshold_act.py foo 2 --tab_name auto2
 
 ## id acc
 #bash ./tools/dist_test.sh custom_config/id_acc/resnet50_topk.py /data/csxjiang/ood_ckpt/pytorch_official/resnet50_custom.pth 2 --metrics accuracy
 #bash ./tools/dist_test_auto.sh custom_config/id_acc/resnet50_topk.py /data/csxjiang/ood_ckpt/pytorch_official/resnet50_custom.pth 2 --metrics accuracy
 #bash ./tools/dist_test_auto.sh custom_config/id_acc/resnet50_topk.py /data/csxjiang/ood_ckpt/mmcls_offical/resnet34_8xb32_in1k_20210831-f257d4e6.pth 2 --metrics accuracy
+bash ./tools/dist_test_auto.sh custom_config/id_acc/resnet50_topk.py /data/csxjiang/ood_ckpt/mmcls_offical/resnet101_8xb32_in1k_20210831-539c63f8.pth 2 --metrics accuracy
