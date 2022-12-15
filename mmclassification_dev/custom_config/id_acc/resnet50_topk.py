6308a14ce696198779be1dd291faf0ff97f1f391
jx <763565607@qq.com>
2022-12-15 22:17:44 +0800

    jx resnet act

--- mmclassification_dev/custom_config/id_acc/resnet50_topk.py
+++ mmclassification_dev/custom_config/id_acc/resnet50_topk.py
@@ -15,8 +15,8 @@ model = dict(
     neck=dict(type='GlobalAveragePooling'),
     head=dict(
         type='LinearClsHead',
-        num_classes=512,
-        in_channels=2048,
+        num_classes=1000,
+        in_channels=512,
         loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
         topk=(1, 5)))
 
