2b5c2ac7a2a84c979ac4aeca3422361ac8ea6ec8
jx <763565607@qq.com>
2022-12-15 22:20:16 +0800

    jx resnet act

--- mmclassification_dev/custom_config/resnet50.py
+++ mmclassification_dev/custom_config/resnet50.py
@@ -28,16 +28,17 @@ model = dict(
         classifier=dict(
         type='ImageClassifier',
         # init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/pytorch_official/resnet50_custom.pth'),
-        init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/mmcls_offical/resnet34_8xb32_in1k_20210831-f257d4e6.pth'),
+        # init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/mmcls_offical/resnet34_8xb32_in1k_20210831-f257d4e6.pth'),
+        init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/mmcls_offical/resnet101_8xb32_in1k_20210831-539c63f8.pth'),
         backbone=dict(
             type='ResNetActivation',
-            depth=34,
+            depth=101,
             num_stages=4,
             out_indices=(3,),
             style='pytorch',
-            th_act_k=0.4,
+            th_act_k=0.1,
             th_act_stage=2,  ## 0:C2 1:C3 2:C4 3:C5
-            th_act_location=5, ## No. of conv layer
+            th_act_location=22, ## No. of conv layer
             feature_sim_stage=3,  ## 0:C2 1:C3 2:C4 3:C5
             feature_sim_location=2,  ## No. of conv layer
         ),
