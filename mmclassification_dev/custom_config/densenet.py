7a2e96f7adb40a335e93fe46fe64ec5cac31595b
jx <763565607@qq.com>
2022-12-17 22:20:50 +0800

    jx densenet act

--- mmclassification_dev/custom_config/densenet.py
+++ mmclassification_dev/custom_config/densenet.py
@@ -27,9 +27,9 @@ model = dict(
         backbone=dict(
             type='DenseNet',
             arch='121',
-            # th_act_k=0.2,
-            # th_act_stage=2,  ## 0:C2 1:C3 2:C4 3:C5
-            # th_act_location=23, ## No. of conv layer   # 121: [6, 12, 24, 16]
+            th_act_k=0.2,
+            th_act_stage=2,  ## 0:C2 1:C3 2:C4 3:C5
+            th_act_location=23, ## No. of conv layer   # 121: [6, 12, 24, 16]
             feature_sim_stage=3,  ## 0:C2 1:C3 2:C4 3:C5
             feature_sim_location=15,  ## No. of conv layer
         ),
