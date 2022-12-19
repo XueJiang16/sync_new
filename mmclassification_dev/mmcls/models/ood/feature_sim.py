b398e28dd18381ea60d95953c303812f6582c8a0
jx <763565607@qq.com>
2022-12-19 22:44:09 +0800

    jx densenet act

--- mmclassification_dev/mmcls/models/ood/feature_sim.py
+++ mmclassification_dev/mmcls/models/ood/feature_sim.py
@@ -39,13 +39,12 @@ class FeatureReweight(BaseModule):
             if self.mode == 'mean':
                 feature_crops = feature_c5.flatten(2)
                 # feature_crops = feature_crops[:,::4].contiguous()
-                value, index = feature_crops.mean(-1).max(dim=-1)  # (N, C, H*W) -> (N, C)
-                patch_sim = torch.zeros_like(value)
-                for i,j in enumerate(index):
-                    patch_sim[i] = torch.abs(feature_crops[i,j] - value[i]).mean(dim=-1)
-
-                # patch_mean = feature_crops.mean(-1).unsqueeze(-1)  # (N, C, H*W) -> (N, C)
-                # patch_sim = torch.abs(feature_crops - patch_mean).mean(dim=(-1, -2))  # for ID: .mean(dim=-2)
+                # value, index = feature_crops.mean(-1).max(dim=-1)  # (N, C, H*W) -> (N, C)
+                # patch_sim = torch.zeros_like(value)
+                # for i,j in enumerate(index):
+                #     patch_sim[i] = torch.abs(feature_crops[i,j] - value[i]).mean(dim=-1)
+                patch_mean = feature_crops.mean(-1).unsqueeze(-1)  # (N, C, H*W) -> (N, C)
+                patch_sim = torch.abs(feature_crops - patch_mean).mean(dim=(-1, -2))  # for ID: .mean(dim=-2)
             elif self.mode == 'channel_mean':
                 feature_crops = feature_c5.flatten(2)
                 patch_mean = feature_crops.mean(1).unsqueeze(1)  # (N, C, H*W) -> (N, C)
