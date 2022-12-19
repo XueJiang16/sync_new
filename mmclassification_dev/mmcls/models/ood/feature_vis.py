5f608a23b0a477f84ac03c2a5548450c2c247928
jx <763565607@qq.com>
2022-12-19 23:38:46 +0800

    jx densenet act

--- mmclassification_dev/mmcls/models/ood/feature_vis.py
+++ mmclassification_dev/mmcls/models/ood/feature_vis.py
@@ -178,11 +178,9 @@ class FeatureVisBlock(BaseModule):
 
             feature_norm1 = self.norm(features1, 1.06)
             feature_norm2 = self.norm(features2, 1.94)
-            print(feature_mean1.shape)
-            assert False
 
-            feature_diff1 = torch.abs(features1 - feature_mean1.unsqueeze(-1).unsqueeze(-1))
-            feature_diff2 = torch.abs(features2 - feature_mean2.unsqueeze(-1).unsqueeze(-1))
+            feature_diff1 = torch.abs(features1 - feature_mean1.unsqueeze(-1))
+            feature_diff2 = torch.abs(features2 - feature_mean2.unsqueeze(-1))
             feature_diff1 = self.norm(feature_diff1, 1.06)
             feature_diff2 = self.norm(feature_diff2, 1.94)
 
