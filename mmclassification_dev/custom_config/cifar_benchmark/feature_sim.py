fcf35e4397c93e0c64dc7b090fc99481c6b09bdc
1@whughw.com <ghw970403>
2022-12-22 00:26:12 +0800

    cifar config update - feature_sim

--- mmclassification_dev/custom_config/cifar_benchmark/feature_sim.py
+++ mmclassification_dev/custom_config/cifar_benchmark/feature_sim.py
@@ -32,8 +32,8 @@ model = dict(
                 # th_act_k=0.05,
                 # th_act_stage=2,  ## 0:C2 1:C3 2:C4 3:C5
                 # th_act_location=5, ## No. of conv layer
-                feature_sim_stage=2,  ## 0:C2 1:C3 2:C4 3:C5
-                feature_sim_location=5,  ## No. of conv layer
+                feature_sim_stage=3,  ## 0:C2 1:C3 2:C4 3:C5
+                feature_sim_location=2,  ## No. of conv layer
                 out_indices=(3,),
                 style='pytorch'),
             # neck=dict(type='GlobalAveragePooling'),
