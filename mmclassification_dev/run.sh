59224fcecb683b27fd1744caf3f5323bb21a273b
jx <763565607@qq.com>
2022-12-22 19:12:51 +0800

    jx cifar benchmark

--- mmclassification_dev/run.sh
+++ mmclassification_dev/run.sh
@@ -24,7 +24,7 @@
 #bash ./tools/ood_dist_test.sh custom_config/densenet.py foo 2
 
 ## Cifar
-#bash ./tools/ood_dist_test.sh custom_config/cifar_benchmark/resnet50.py foo 1
+#bash ./tools/ood_dist_test.sh custom_config/cifar_benchmark/resnet50.py foo 2
 
 ## google doc
 #bash ./tools/ood_dist_test_auto.sh custom_config/patchsim.py foo 2 --tab_name auto3
