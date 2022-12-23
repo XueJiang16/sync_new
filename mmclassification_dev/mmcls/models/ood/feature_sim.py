dbaabd985be68ba8a1200a3736f256f42669bfb2
jx <763565607@qq.com>
2022-12-23 17:12:37 +0800

    jx feature sim

--- mmclassification_dev/mmcls/models/ood/feature_sim.py
+++ mmclassification_dev/mmcls/models/ood/feature_sim.py
@@ -96,7 +96,7 @@ class FeatureReweight(BaseModule):
                         plt.xlabel(r"X")
                         plt.ylabel(r"Density")
                         plt.legend()
-                        plt.savefig("test.png")
+                        plt.savefig("test_pic{}_channel{}.png".format(i,j))
                         plt.close('all')
                         # print(x.shape)
                         assert False
