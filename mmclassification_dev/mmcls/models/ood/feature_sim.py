c371847e56c499c2770b5be3533a6e80965aa86b
jx <763565607@qq.com>
2022-12-23 20:28:04 +0800

    jx feature sim

--- mmclassification_dev/mmcls/models/ood/feature_sim.py
+++ mmclassification_dev/mmcls/models/ood/feature_sim.py
@@ -70,7 +70,8 @@ class FeatureReweight(BaseModule):
                 feature_crops = feature_c5.flatten(2)
                 batch_size, channel, _ = feature_crops.shape
                 for i in range(batch_size):
-                    x = feature_crops[i].mean(0)
+                    # x = feature_crops[i].mean(0)
+                    x = feature_crops[i].flatten(0)
                     x = x.cpu().detach().numpy()
                     # for j in range(channel):
                     #     x = feature_crops[i,j].cpu().detach().numpy()
@@ -86,14 +87,14 @@ class FeatureReweight(BaseModule):
                     #       format(float(mean[1][0]), np.sqrt(float(covs[1][0][0])), weights[1]))
 
                     # create necessary things to plot
-                    x_axis = np.arange(-0.2, 2, 0.01)
+                    x_axis = np.arange(-0.2, 1, 0.01)
                     y_axis0 = norm.pdf(x_axis, float(mean[0][0]), np.sqrt(float(covs[0][0][0]))) * weights[0]  # 1st gaussian
                     y_axis1 = norm.pdf(x_axis, float(mean[1][0]), np.sqrt(float(covs[1][0][0]))) * weights[1]  # 2nd gaussian
                     plt.hist(x, density=True, color='black')
                     plt.plot(x_axis, y_axis0, label='Dis 1')
                     plt.plot(x_axis, y_axis1, label='Dis 2')
                     plt.plot(x_axis, y_axis0 + y_axis1, ls='dashed', label='Mixed Dis')
-                    plt.xlim(-0.2, 2)
+                    plt.xlim(-0.2, 1)
                     # plt.ylim(0.0, 2.0)
                     plt.xlabel(r"X")
                     plt.ylabel(r"Density")
