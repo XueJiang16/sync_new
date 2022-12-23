26860d0e0bf2af7767628400dfbd997ba3c2e22e
jx <763565607@qq.com>
2022-12-23 20:45:57 +0800

    jx feature sim

--- mmclassification_dev/mmcls/models/ood/feature_sim.py
+++ mmclassification_dev/mmcls/models/ood/feature_sim.py
@@ -81,17 +81,18 @@ class FeatureReweight(BaseModule):
                     mean = gmm.fit(x).means_
                     covs = gmm.fit(x).covariances_
                     weights = gmm.fit(x).weights_
-                    print('Dis1 mean={}, std={}, weight={}'.
-                          format(float(mean[0][0]), np.sqrt(float(covs[0][0][0])), weights[0]))
-                    print('Dis2 mean={}, std={}, weight={}'.
-                          format(float(mean[1][0]), np.sqrt(float(covs[1][0][0])), weights[1]))
+                    # print('Dis1 mean={}, std={}, weight={}'.
+                    #       format(float(mean[0][0]), np.sqrt(float(covs[0][0][0])), weights[0]))
+                    # print('Dis2 mean={}, std={}, weight={}'.
+                    #       format(float(mean[1][0]), np.sqrt(float(covs[1][0][0])), weights[1]))
 
                     # create necessary things to plot
                     x_axis = np.arange(-0.05, 0.05, 0.0000001)
                     y_axis0 = norm.pdf(x_axis, float(mean[0][0]), np.sqrt(float(covs[0][0][0]))) * weights[0]  # 1st gaussian
                     y_axis1 = norm.pdf(x_axis, float(mean[1][0]), np.sqrt(float(covs[1][0][0]))) * weights[1]  # 2nd gaussian
+                    print(y_axis0.max())
                     plt.hist(x, density=True, color='black', bins=30)
-                    plt.plot(x_axis, y_axis0, label='Dis 1')
+                    # plt.plot(x_axis, y_axis0, label='Dis 1')
                     plt.plot(x_axis, y_axis1, label='Dis 2')
                     # plt.plot(x_axis, y_axis0 + y_axis1, ls='dashed', label='Mixed Dis')
                     # plt.xlim(-0.05, 0.05)
