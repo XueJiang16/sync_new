fb70a9a497e703a55fa56a636d3edb9bd90ac4cb
jx <763565607@qq.com>
2022-12-23 16:49:14 +0800

    jx feature sim

--- mmclassification_dev/mmcls/models/ood/feature_sim.py
+++ mmclassification_dev/mmcls/models/ood/feature_sim.py
@@ -7,14 +7,14 @@ import numpy as np  # noqa
 from collections import Counter  # noqa
 
 #for gmm
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy import stats
-from matplotlib.ticker import (MultipleLocator, FormatStrFormatter, AutoMinorLocator)
-import astropy
-from scipy.stats import norm
-from sklearn.mixture import GaussianMixture as GMM
-import matplotlib as mpl
+# import matplotlib.pyplot as plt
+# import numpy as np
+# from scipy import stats
+# from matplotlib.ticker import (MultipleLocator, FormatStrFormatter, AutoMinorLocator)
+# import astropy
+# from scipy.stats import norm
+# from sklearn.mixture import GaussianMixture as GMM
+# import matplotlib as mpl
 
 
 from ..builder import OOD
