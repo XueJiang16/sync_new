6bc7c775e2b5d5d91672f11a75b23a72a3fec0a9
jx <763565607@qq.com>
2022-12-23 23:21:49 +0800

    jx feature sim

--- mmclassification_dev/mmcls/models/ood/feature_sim.py
+++ mmclassification_dev/mmcls/models/ood/feature_sim.py
@@ -33,6 +33,58 @@ class FeatureReweight(BaseModule):
             self.ood_detector = no_ood_detector
         self.mode = mode
 
+    def gmm_score(self, x):
+        x = x.cpu().detach().numpy()
+        x = x.reshape(-1, 1)
+        gmm = GMM(n_components=2, max_iter=100, random_state=10, covariance_type='full')
+        # find useful parameters
+        mean = gmm.fit(x).means_
+        covs = gmm.fit(x).covariances_
+        weights = gmm.fit(x).weights_
+
+        # print('Dis1 mean={}, std={}, weight={}'.
+        #       format(float(mean[0][0]), np.sqrt(float(covs[0][0][0])), weights[0]))
+        # print('Dis2 mean={}, std={}, weight={}'.
+        #       format(float(mean[1][0]), np.sqrt(float(covs[1][0][0])), weights[1]))
+
+        # create necessary things to plot
+        # x_axis = np.arange(-0.1, 1.1, 0.001)
+        # y_axis0 = norm.pdf(x_axis, float(mean[0][0]), np.sqrt(float(covs[0][0][0]))) * weights[0]  # 1st gaussian
+        # y_axis1 = norm.pdf(x_axis, float(mean[1][0]), np.sqrt(float(covs[1][0][0]))) * weights[1]  # 2nd gaussian
+        # plt.hist(x, density=True, color='black', bins=60)
+        # plt.plot(x_axis, y_axis0, label='Dis 1')
+        # plt.plot(x_axis, y_axis1, label='Dis 2')
+        # plt.plot(x_axis, y_axis0 + y_axis1, ls='dashed', label='Mixed Dis')
+        # plt.xlim(-0.1, 1.1)
+        # # plt.ylim(0.0, 2.0)
+        # plt.xlabel(r"X")
+        # plt.ylabel(r"Density")
+        # plt.legend()
+        # filename = os.path.splitext(os.path.basename(filenames[i]))[0]
+        # plt.savefig(os.path.join(out_dir, '{}.jpg'.format(filename)))
+        # plt.close('all')
+
+        if mean[0][0] > mean[1][0]:
+            forg_idx = 0
+            bacg_idx = 1
+        else:
+            forg_idx = 1
+            bacg_idx = 0
+        forg_mean = mean[forg_idx][0]
+        forg_std = np.sqrt(float(covs[forg_idx][0][0]))
+        forg_weight = weights[forg_idx]
+        bacg_mean = mean[bacg_idx][0]
+        bacg_std = np.sqrt(float(covs[bacg_idx][0][0]))
+        bacg_weight = weights[bacg_idx]
+
+        # single_score = (forg_mean - bacg_mean) * forg_weight / bacg_weight
+        x_mean = x.mean()
+        gmm_mean = forg_weight * forg_mean + bacg_weight * bacg_mean
+        single_score = np.abs(forg_mean - gmm_mean) * forg_weight
+        # + np.abs(bacg_mean - gmm_mean) * bacg_weight
+        return single_score
+
+
 
     def forward(self, **input):
         if "type" in input:
@@ -81,57 +133,11 @@ class FeatureReweight(BaseModule):
                 score = []
                 for i in range(batch_size):
                     x = feature_crops[i]
-                    x = x.reshape(int(channel/sub_channel), sub_channel, -1)
-                    x = x.mean(1)
-                    x = x.cpu().detach().numpy()
-                    x = x.reshape(-1,1)
-                    gmm = GMM(n_components=2, max_iter=100, random_state=10, covariance_type='full')
-                    # find useful parameters
-                    mean = gmm.fit(x).means_
-                    covs = gmm.fit(x).covariances_
-                    weights = gmm.fit(x).weights_
-
-                    if mean[0][0] > mean[1][0]:
-                        forg_idx = 0
-                        bacg_idx = 1
-                    else:
-                        forg_idx = 1
-                        bacg_idx = 0
-                    forg_mean = mean[forg_idx][0]
-                    forg_std = np.sqrt(float(covs[forg_idx][0][0]))
-                    forg_weight = weights[forg_idx]
-                    bacg_mean = mean[bacg_idx][0]
-                    bacg_std = np.sqrt(float(covs[bacg_idx][0][0]))
-                    bacg_weight = weights[bacg_idx]
-
-                    # single_score = (forg_mean - bacg_mean) * forg_weight / bacg_weight
-                    x_mean = x.mean()
-                    gmm_mean = forg_weight * forg_mean + bacg_weight * bacg_mean
-                    single_score = np.abs(forg_mean - gmm_mean) * forg_weight
-                                   # + np.abs(bacg_mean - gmm_mean) * bacg_weight
+                    # x = x.reshape(int(channel/sub_channel), sub_channel, -1)
+                    # x = x.mean(1)
+                    single_score = np.mean(list(map(self.gmm_score, x)))
+                    # single_score = self.gmm_score(x)
                     score.append(single_score)
-
-                    # print('Dis1 mean={}, std={}, weight={}'.
-                    #       format(float(mean[0][0]), np.sqrt(float(covs[0][0][0])), weights[0]))
-                    # print('Dis2 mean={}, std={}, weight={}'.
-                    #       format(float(mean[1][0]), np.sqrt(float(covs[1][0][0])), weights[1]))
-
-                    # create necessary things to plot
-                    # x_axis = np.arange(-0.1, 1.1, 0.001)
-                    # y_axis0 = norm.pdf(x_axis, float(mean[0][0]), np.sqrt(float(covs[0][0][0]))) * weights[0]  # 1st gaussian
-                    # y_axis1 = norm.pdf(x_axis, float(mean[1][0]), np.sqrt(float(covs[1][0][0]))) * weights[1]  # 2nd gaussian
-                    # plt.hist(x, density=True, color='black', bins=60)
-                    # plt.plot(x_axis, y_axis0, label='Dis 1')
-                    # plt.plot(x_axis, y_axis1, label='Dis 2')
-                    # plt.plot(x_axis, y_axis0 + y_axis1, ls='dashed', label='Mixed Dis')
-                    # plt.xlim(-0.1, 1.1)
-                    # # plt.ylim(0.0, 2.0)
-                    # plt.xlabel(r"X")
-                    # plt.ylabel(r"Density")
-                    # plt.legend()
-                    # filename = os.path.splitext(os.path.basename(filenames[i]))[0]
-                    # plt.savefig(os.path.join(out_dir, '{}.jpg'.format(filename)))
-                    # plt.close('all')
                 patch_sim = torch.tensor(score).to("cuda:{}".format(self.local_rank))
 
 
