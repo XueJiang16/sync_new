79312c00a8eb05c0c3e8f75eeb327c08943da3da
jx <763565607@qq.com>
2022-12-30 00:00:17 +0800

    jx feature sim

--- mmclassification_dev/vis/vis_feature_pdf.py
+++ mmclassification_dev/vis/vis_feature_pdf.py
@@ -52,9 +52,9 @@ def oversample(x, k):
     # x = x.mean(0).flatten()
     x_mean = x.mean(dim=(1,2))
     value, idx = torch.topk(x_mean, k)
+    x = x[idx[-1]]
     print(x.shape)
     exit()
-    x = x[idx[-1]]
     x = torch.nn.functional.interpolate(x, (128,128))
     return x
 
