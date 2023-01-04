7d4da7b2b5e2bece772528be061f429851ecd72d
jx <763565607@qq.com>
2023-01-04 22:58:16 +0800

    jx vit

--- mmclassification_dev/run.sh
+++ mmclassification_dev/run.sh
@@ -6,7 +6,7 @@
 #bash ./tools/ood_dist_test.sh custom_config/Noise/resnet50_uniform.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/Noise/resnet50_gaussian.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/Noise/resnet50_colorband.py foo 
-bash ./tools/ood_dist_test.sh custom_config/resnet50.py foo 2
+#bash ./tools/ood_dist_test.sh custom_config/resnet50.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/resnet50_official.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/meanstd.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/patchsim.py foo 2
@@ -22,7 +22,7 @@ bash ./tools/ood_dist_test.sh custom_config/resnet50.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/vit_lt.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/patchsim_mobile.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/densenet.py foo 2
-
+bash ./tools/ood_dist_test.sh custom_config/vit.py foo 2
 ## Cifar
 #bash ./tools/ood_dist_test.sh custom_config/cifar_benchmark/resnet50.py foo 2
 
