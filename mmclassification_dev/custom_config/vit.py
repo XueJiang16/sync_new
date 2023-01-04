28ae97e7638453a730270cdb77ccf96f83f392a2
jx <763565607@qq.com>
2023-01-04 23:12:40 +0800

    jx vit

--- mmclassification_dev/custom_config/vit.py
+++ mmclassification_dev/custom_config/vit.py
@@ -3,7 +3,6 @@ import os
 method_list = ["GradNorm", "MSP", "Energy", "ODIN"]
 method_name = method_list[1]
 model_name = 'vit'
-train_dataset = 'a8'
 # info=os.path.expanduser('/data/csxjiang/dice_cache/imagenet_res101_{}_feature_stat.pth'.format(train_dataset))
 
 custom_name = None
