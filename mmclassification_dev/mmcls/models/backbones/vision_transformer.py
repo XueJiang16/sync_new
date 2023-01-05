a757bd7677be02bafe647f9a334817d489afaf2a
1@whughw.com <ghw970403>
2023-01-05 23:49:10 +0800

    ghw cls_token as feature affinity

--- mmclassification_dev/mmcls/models/backbones/vision_transformer.py
+++ mmclassification_dev/mmcls/models/backbones/vision_transformer.py
@@ -377,8 +377,8 @@ class VisionTransformer(BaseBackbone):
                 if self.output_cls_token:
                     out = [patch_token, cls_token]
                 else:
-                    # out = patch_token
-                    out = x.permute((0, 2, 1))
+                    out = patch_token
+                    # out = x.permute((0, 2, 1))
                 outs.append(out)
 
         return tuple(outs)
--- mmclassification_dev/mmcls/models/ood/feature_sim.py
+++ mmclassification_dev/mmcls/models/ood/feature_sim.py
@@ -178,22 +178,22 @@ class FeatureReweight(BaseModule):
             if self.mode == 'vit':
                 # feature_c5 (B, 768, 24, 24)
                 feature_tokens = feature_c5.flatten(2)  # (B, 768, 576)
-                # feature_tokens = feature_tokens.permute((0, 2, 1))  # (B, 576, 768)
+                feature_tokens = feature_tokens.permute((0, 2, 1))  # (B, 576, 768)
                 feature_tokens_ = feature_tokens / feature_tokens.norm(dim=-1).unsqueeze(-1)  # (B, 576, 768N)
-                cls_token, patch_token = feature_tokens_[:, :, 0].unsqueeze(-1), feature_tokens_[:, :, 1:]
-                feature_affinity = torch.einsum("bdi,bdj->bij", cls_token, patch_token)
-                # feature_affinity = torch.einsum("bid,bjd->bij", feature_tokens_, feature_tokens_)  # (B, 576, 576)
-                filenames = [x['filename'] for x in input['img_metas']]
-                output_path = "./feature_affinity_vis"
-                os.makedirs(output_path, exist_ok=True)
-                for i in range(len(feature_tokens)):
-                    img_name = filenames[i]
-                    img = cv2.imread(img_name)
-                    # f = feature_affinity[i, 288].reshape((24, 24)).cpu()
-                    f = feature_affinity[i, 0].reshape((24, 24)).cpu()
-                    f = (f + 1) / 2
-                    cam = show_heatmap(img, f)
-                    cv2.imwrite(os.path.join(output_path, os.path.basename(img_name)), cam)
+                # cls_token, patch_token = feature_tokens_[:, :, 0].unsqueeze(-1), feature_tokens_[:, :, 1:]
+                # feature_affinity = torch.einsum("bdi,bdj->bij", cls_token, patch_token)
+                feature_affinity = torch.einsum("bid,bjd->bij", feature_tokens_, feature_tokens_)  # (B, 576, 576)
+                # filenames = [x['filename'] for x in input['img_metas']]
+                # output_path = "./feature_affinity_vis"
+                # os.makedirs(output_path, exist_ok=True)
+                # for i in range(len(feature_tokens)):
+                #     img_name = filenames[i]
+                #     img = cv2.imread(img_name)
+                #     # f = feature_affinity[i, 288].reshape((24, 24)).cpu()
+                #     f = feature_affinity[i, 0].reshape((24, 24)).cpu()
+                #     f = (f + 1) / 2
+                #     cam = show_heatmap(img, f)
+                #     cv2.imwrite(os.path.join(output_path, os.path.basename(img_name)), cam)
                 feature_crops = feature_affinity
                 patch_mean = feature_crops.mean(-1).unsqueeze(-1)  # (N, C, H*W) -> (N, C)
                 patch_sim = torch.abs(feature_crops - patch_mean).mean(dim=(-1, -2))  # for ID: .mean(dim=-2)
