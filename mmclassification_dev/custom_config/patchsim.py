06fbaefbe4c61ada6ccade2727dba400723be3e9
jx <763565607@qq.com>
2023-01-07 23:30:46 +0800

    jx patchsim

--- mmclassification_dev/custom_config/patchsim.py
+++ mmclassification_dev/custom_config/patchsim.py
@@ -18,10 +18,10 @@ model = dict(
     threshold=0.4,
     order=1,
     # k=k_c5,
-    mode='mean',
+    mode=None,
     fuse_const=1,
     ood_detector=dict(
-        type=method_list[2],
+        type=method_list[3],
         debug_mode=False,
         num_classes=1000,
         # temperature=1,
@@ -37,7 +37,7 @@ model = dict(
                 out_indices=(3,),
                 style='pytorch',
                 random_block=[1],
-                random_block_k=[0.14],
+                random_block_k=[0.1],
                 random_block_location=[2],  # 0:C2 1:C3 2:C4 3:C5
             ),
             neck=dict(type='GlobalAveragePooling'),
