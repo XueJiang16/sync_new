7db9f64d7280b89ba6d62ff0e09665e879d1b03b
jx <763565607@qq.com>
2023-01-10 16:24:33 +0800

    jx vis gmm

--- mmclassification_dev/mmcls/models/ood/feature_sim.py
+++ mmclassification_dev/mmcls/models/ood/feature_sim.py
@@ -85,12 +85,12 @@ class FeatureReweight(BaseModule):
         y_axis1 = norm.pdf(x_axis, float(mean[1][0]), np.sqrt(float(covs[1][0][0]))) * weights[1]  # 2nd gaussian
         plt.hist(x, density=True, color='black', bins=60)
         if mean[0][0] > mean[1][0]:
-            plt.plot(x_axis, y_axis0, label='Foreground')
-            plt.plot(x_axis, y_axis1, label='Background')
+            plt.plot(x_axis, y_axis0, label='Foreground', color='darkorange')
+            plt.plot(x_axis, y_axis1, label='Background', color='g')
         else:
-            plt.plot(x_axis, y_axis0, label='Background')
-            plt.plot(x_axis, y_axis1, label='Foreground')
-        plt.plot(x_axis, y_axis0 + y_axis1, ls='dashed', label='Mixed Distribution')
+            plt.plot(x_axis, y_axis0, label='Background', color='g')
+            plt.plot(x_axis, y_axis1, label='Foreground', color='darkorange')
+        plt.plot(x_axis, y_axis0 + y_axis1, ls='dashed', label='Mixed Distribution', color='b')
         plt.xlim(-0.1, 1.1)
         # plt.ylim(0.0, 2.0)
         plt.xlabel(r"X")
