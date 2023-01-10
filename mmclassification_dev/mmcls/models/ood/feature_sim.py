6bf66ac6024242c11ce11a396fc607c02fe7c45a
jx <763565607@qq.com>
2023-01-10 16:29:27 +0800

    jx vis gmm

--- mmclassification_dev/mmcls/models/ood/feature_sim.py
+++ mmclassification_dev/mmcls/models/ood/feature_sim.py
@@ -86,11 +86,11 @@ class FeatureReweight(BaseModule):
         plt.hist(x, density=True, color='black', bins=60)
         if mean[0][0] > mean[1][0]:
             plt.plot(x_axis, y_axis0, label='Foreground', color='darkorange')
-            plt.plot(x_axis, y_axis1, label='Background', color='g')
+            plt.plot(x_axis, y_axis1, label='Background', color='royalblue')
         else:
-            plt.plot(x_axis, y_axis0, label='Background', color='g')
+            plt.plot(x_axis, y_axis0, label='Background', color='royalblue')
             plt.plot(x_axis, y_axis1, label='Foreground', color='darkorange')
-        plt.plot(x_axis, y_axis0 + y_axis1, ls='dashed', label='Mixed Distribution', color='royalblue')
+        plt.plot(x_axis, y_axis0 + y_axis1, ls='dashed', label='Mixed Distribution', color='g')
         plt.xlim(-0.1, 1.1)
         # plt.ylim(0.0, 2.0)
         plt.xlabel(r"X")
@@ -98,7 +98,7 @@ class FeatureReweight(BaseModule):
         plt.legend()
         plt.tight_layout()
 
-        plt.savefig(os.path.join(out_dir, '{}.jpg'.format(filename)))
+        plt.savefig(os.path.join(out_dir, '{}.pdf'.format(filename)))
         plt.close('all')
 
         if mean[0][0] > mean[1][0]:
