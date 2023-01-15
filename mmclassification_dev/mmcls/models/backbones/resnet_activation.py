9dfd9ea09858079fc020e07645583d2be6c829c4
jx <763565607@qq.com>
2023-01-15 23:51:27 +0800

    jx spurious

--- mmclassification_dev/mmcls/models/backbones/resnet_activation.py
+++ mmclassification_dev/mmcls/models/backbones/resnet_activation.py
@@ -682,8 +682,8 @@ class ResNetActivation(BaseBackbone):
             res_layer = getattr(self, layer_name)
             x = res_layer(x, th_act_para=th_act_parameter,
                           feature_sim_para=feature_sim_parameter, th_act_k=self.th_act_k, stage=i)
-            # if i == self.feature_sim_stage:
-            #     return tuple([x])
+            if i == self.feature_sim_stage:
+                return tuple([x])
             if i in self.out_indices:
                 outs.append(x)
         return tuple(outs)
