0b88dca3b55510920eedbc51728ff8d773c1f96b
jx <763565607@qq.com>
2023-01-23 00:00:48 +0800

    jx vis

--- mmclassification_dev/vis/figure/fig1.py
+++ mmclassification_dev/vis/figure/fig1.py
@@ -99,10 +99,10 @@ img_paths = ["/data/csxjiang/val", '/data/csxjiang/ood_data/iNaturalist/images',
             '/data/csxjiang/ood_data/Places/images', '/data/csxjiang/ood_data/Textures/dtd/images_collate']
 # img_names = ['ID', 'iNaturalist', 'SUN', 'Places', 'Textures']
 
-# i=0
-# img_name = 'ILSVRC2012_val_00011372.JPEG'
-i = 3
-img_name = 'f_field_wild_00000123.jpg'
+i=0
+img_name = 'ILSVRC2012_val_00011372.JPEG'
+# i = 3
+# img_name = 'f_field_wild_00000123.jpg'
 img_path = img_paths[i]
 dst_path = "./vis/figure/fig1/"
 os.makedirs(dst_path, exist_ok=True)
