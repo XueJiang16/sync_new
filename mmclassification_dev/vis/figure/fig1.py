e1a9b2a0a89fad3aa0d7278300a9257f1ff06d04
jx <763565607@qq.com>
2023-01-22 17:10:58 +0800

    jx vis

--- mmclassification_dev/vis/figure/fig1.py
+++ mmclassification_dev/vis/figure/fig1.py
@@ -138,7 +138,7 @@ with torch.no_grad():
     c5_norm_mask = np.float32(c5_norm_mask) / 255
     c5_norm_mask = np.reshape(c5_norm_mask, newshape=-1)
     print(c5_norm_mask.shape)
-    plt.hist(c5_norm_mask, density=True, bins=100)
+    plt.hist(c5_norm_mask, density=True, bins=20)
     plt.savefig("{}_before_hist.jpg".format(os.path.join(dst_path, os.path.splitext(img_name)[0])))
     plt.close()
 
