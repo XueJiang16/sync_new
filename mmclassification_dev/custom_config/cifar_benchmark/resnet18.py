15b0e373d3bebc25fde0e7ff57bb0a55546560d7
jx <763565607@qq.com>
2023-03-26 18:19:45 +0800

    jx rebuttal near ood

--- mmclassification_dev/custom_config/cifar_benchmark/resnet18.py
+++ mmclassification_dev/custom_config/cifar_benchmark/resnet18.py
@@ -38,21 +38,21 @@ model = dict(
         #     in_channels=512,
         #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
         #     topk=(1, 5)),
-        # head=dict(
-        #     type='DiceHead',
-        #     num_classes=num_classes,
-        #     in_channels=512,
-        #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
-        #     topk=(1, 5),
-        #     info=info,
-        #     p=0.9,),
         head=dict(
-            type='ReactHead',
+            type='DiceHead',
             num_classes=num_classes,
             in_channels=512,
-            threshold=1,
             loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
-            topk=(1, 5))
+            topk=(1, 5),
+            info=info,
+            p=0.9,),
+        # head=dict(
+        #     type='ReactHead',
+        #     num_classes=num_classes,
+        #     in_channels=512,
+        #     threshold=1,
+        #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        #     topk=(1, 5))
 
     )
 )
