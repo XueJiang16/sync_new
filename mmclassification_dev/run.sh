d1d0ec1c0ad2d57287af58e7126e092a215e33e9
jx <763565607@qq.com>
2023-08-03 16:14:07 +0800

    jx fs+ta init

--- mmclassification_dev/run.sh
+++ mmclassification_dev/run.sh
@@ -6,10 +6,10 @@
 #bash ./tools/ood_dist_test.sh custom_config/Noise/resnet50_uniform.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/Noise/resnet50_gaussian.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/Noise/resnet50_colorband.py foo 
-bash ./tools/ood_dist_test.sh custom_config/resnet50.py foo 2
+#bash ./tools/ood_dist_test.sh custom_config/resnet50.py foo 2
 # bash ./tools/ood_dist_test.sh custom_config/resnet50_official.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/meanstd.py foo 2
-#bash ./tools/ood_dist_test.sh custom_config/patchsim.py foo 2
+bash ./tools/ood_dist_test.sh custom_config/patchsim.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/inat_ib/resnet101.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/aug_contrast.py foo 2
 #bash ./tools/ood_dist_test.sh custom_config/cifar10.py foo 2
