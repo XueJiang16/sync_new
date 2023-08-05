569d5176eec7f414525aee089c4ce91b4f68a570
jx <763565607@qq.com>
2023-08-05 15:06:14 +0800

    jx fs+ta init

--- mmclassification_dev/custom_config/patchsim.py
+++ mmclassification_dev/custom_config/patchsim.py
@@ -37,7 +37,7 @@ model = dict(
                 out_indices=(3,),
                 style='pytorch',
                 random_block=[1],
-                random_block_k=[0.15],
+                random_block_k=[0],
                 random_block_location=[2],  # 0:C2 1:C3 2:C4 3:C5
             ),
             neck=dict(type='GlobalAveragePooling'),
