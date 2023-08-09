9b985d672cdb80fa08e10c569182af7842815c61
jx <763565607@qq.com>
2023-08-09 17:21:54 +0800

    jx openood fs+ta

--- OpenOOD/openood/postprocessors/fs_postprocessor.py
+++ OpenOOD/openood/postprocessors/fs_postprocessor.py
@@ -12,12 +12,12 @@ class FSPostprocessor(BasePostprocessor):
     def __init__(self, config):
         super(FSPostprocessor, self).__init__(config)
         self.args = self.config.postprocessor.postprocessor_args
-        self.thred = self.args.thred
+        self.thred = self.args.thred # search hyper
         self.args_dict = self.config.postprocessor.postprocessor_sweep
 
     @torch.no_grad()
     def postprocess(self, net: nn.Module, data: Any):
-        feature5, output = net.forward_ta(data, ta_thred=self.thred)
+        feature5, output = net.forward_ta(data, ta_thred=0.2)
         _, pred = torch.max(output, dim=1)
         energyconf = torch.logsumexp(output.data.cpu(), dim=1)
 
@@ -25,7 +25,7 @@ class FSPostprocessor(BasePostprocessor):
         feature_crops = feature5.flatten(2)
         patch_mean = feature_crops.mean(-1).unsqueeze(-1)  # (N, C, H*W) -> (N, C)
         fs_conf = torch.abs(feature_crops - patch_mean).mean(dim=(-1, -2))  # for ID: .mean(dim=-2)
-        conf = fs_conf
+        conf = fs_conf + self.thred * energyconf
         return pred, conf
 
     def set_hyperparam(self, hyperparam: list):
