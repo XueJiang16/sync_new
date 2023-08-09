d86e85b1c8db21789b918e9a952ef966a4ba768b
jx <763565607@qq.com>
2023-08-09 16:55:03 +0800

    jx openood fs+ta

--- OpenOOD/openood/evaluation_api/postprocessor.py
+++ OpenOOD/openood/evaluation_api/postprocessor.py
@@ -13,7 +13,7 @@ from openood.postprocessors import (
     Rd4adPostprocessor, ReactPostprocessor, ResidualPostprocessor,
     SSDPostprocessor, TemperatureScalingPostprocessor, VIMPostprocessor,
     RotPredPostprocessor, RankFeatPostprocessor, RMDSPostprocessor,
-    SHEPostprocessor, CIDERPostprocessor, NPOSPostprocessor)
+    SHEPostprocessor, CIDERPostprocessor, NPOSPostprocessor, FSPostprocessor)
 from openood.utils.config import Config, merge_configs
 
 postprocessors = {
@@ -54,7 +54,8 @@ postprocessors = {
     'she': SHEPostprocessor,
     'rd4ad': Rd4adPostprocessor,
     'rotpred': RotPredPostprocessor,
-    'rankfeat': RankFeatPostprocessor
+    'rankfeat': RankFeatPostprocessor,
+    'fsta': FSPostprocessor,
 }
 
 link_prefix = 'https://raw.githubusercontent.com/Jingkang50/OpenOOD/main/configs/postprocessors/'
