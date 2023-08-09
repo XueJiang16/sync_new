4de649a5aebfd71eff28d2d2adb63deecf2f4fc5
jx <763565607@qq.com>
2023-08-09 18:44:33 +0800

    jx openood fs+ta+react

--- OpenOOD/openood/postprocessors/fs_postprocessor.py
+++ OpenOOD/openood/postprocessors/fs_postprocessor.py
@@ -26,7 +26,7 @@ class FSPostprocessor(BasePostprocessor):
         patch_mean = feature_crops.mean(-1).unsqueeze(-1)  # (N, C, H*W) -> (N, C)
         fs_conf = torch.abs(feature_crops - patch_mean).mean(dim=(-1, -2))  # for ID: .mean(dim=-2)
         fs_conf = fs_conf.data.cpu()
-        conf = fs_conf + self.thred * energyconf
+        conf = fs_conf + 0.0028 * energyconf
         return pred, conf
 
     def set_hyperparam(self, hyperparam: list):
