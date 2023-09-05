99017866b04c395cd044f5562e2559ade1c5e66d
xuejiang <j763565607@gmail.com>
2023-09-05 17:34:28 +0800

    jx vis hyper line

--- mmclassification_dev/vis/figure/vis_hyper_line.py
+++ mmclassification_dev/vis/figure/vis_hyper_line.py
@@ -6,8 +6,8 @@ import seaborn as sns
 sns.set_style("whitegrid")
 sns.set_palette("deep")
 # 横坐标和纵坐标数据
-x_values = [0.01, 0.012, 0.014, 0.016, 0.018, 0.02, 0.022, 0.024, 0.026, 0.028, 0.03, 0.008, 0.006]
-y_values = [94.91, 95.01, 95.06, 95.08, 95.07, 95.05, 95.01, 94.96, 94.91, 94.84, 94.78, 94.76, 94.52]
+x_values = [500, 600, 700, 800, 900, 1000, 1100, 1200, 1300]
+y_values = [93.21, 93.26, 93.30, 93.35, 93.39, 93.44, 93.48, 93.52, 93.57]
 
 x_values, y_values = zip(*sorted(zip(x_values, y_values)))
 
@@ -16,7 +16,7 @@ plt.figure(figsize=(8, 6))  # 设置图形大小
 plt.plot(x_values, y_values, marker='o', linestyle='-', markersize=6)
 
 # 添加标题和标签
-plt.title("FS+TA+Energy", fontsize=16)
+plt.title("FS+TA+ODIN", fontsize=16)
 plt.xlabel("λ", fontsize=14)
 plt.ylabel("AUROC (%)", fontsize=14)
 
@@ -25,7 +25,7 @@ plt.xticks(x_values, fontsize=12, rotation=45)
 
 # 设置纵坐标刻度
 plt.yticks(fontsize=12)
-plt.ylim(92, 98)
+plt.ylim(90, 96)
 
 
 # 添加网格线
@@ -34,7 +34,7 @@ plt.tight_layout()
 # 显示图例
 
 # 保存图像为文件（可选）
-plt.savefig("hyper1.pdf")
+plt.savefig("hyper2.pdf")
 
 # # 显示图像
 # plt.show()
