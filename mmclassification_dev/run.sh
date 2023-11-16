1a9935295681f089cf5c6b04de02706b3460a289
xuejiang <j763565607@gmail.com>
2023-11-16 17:54:04 +0800

    jx test mnist fs+ta

--- mmclassification_dev/run.sh
+++ mmclassification_dev/run.sh
@@ -30,7 +30,9 @@
 
 ## Cifar
 #bash ./tools/ood_dist_test.sh custom_config/cifar_benchmark/resnet50.py foo 2
-bash ./tools/ood_dist_test.sh custom_config/cifar_benchmark/resnet18.py foo 2
+#bash ./tools/ood_dist_test.sh custom_config/cifar_benchmark/resnet18.py foo 2
+bash ./tools/ood_dist_test.sh custom_config/cifar_benchmark/feature_sim.py foo 2
+
 
 
 ##coco
