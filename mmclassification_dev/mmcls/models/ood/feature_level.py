81f6aa05651378f09ff9ff3690b16ef6a6f3aa07
jx <763565607@qq.com>
2023-11-20 21:09:22 +0800

    jx gram matrix

--- mmclassification_dev/mmcls/models/ood/feature_level.py
+++ mmclassification_dev/mmcls/models/ood/feature_level.py
@@ -164,7 +164,7 @@ class FeatureMapSim(BaseModule):
                 # patch_sim = torch.abs(feature_crops - patch_mean).mean(dim=(-1, -2))  # for ID: .mean(dim=-2)
 
                 # gram matrix
-                patch_sim = torch.einsum("nlc,ncl->nll", feature_crops.permute((0,2,1)), feature_crops).mean(dim=(-1, -2))
+                patch_sim = torch.einsum("nlc,ncd->nld", feature_crops.permute((0,2,1)), feature_crops).mean(dim=(-1, -2))
 
                 # c5_sum = feature_c5.sum(dim=[1,2,3])
                 # ratio = (c5_sum - 20000) / 10000
