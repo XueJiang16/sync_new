f99f71104fb6e75810dc27b3039970ebd368f72f
ghw <haowenguo@tencent.com>
2023-11-22 20:42:11 +0800

    disable gram matrix

--- mmclassification_dev/mmcls/models/ood/feature_level.py
+++ mmclassification_dev/mmcls/models/ood/feature_level.py
@@ -160,15 +160,15 @@ class FeatureMapSim(BaseModule):
             elif self.mode == 'mean':
                 # feature_c5 = feature_c5[:,:,1:6,1:6]
                 feature_crops = feature_c5.flatten(2)
-                # patch_mean = feature_crops.mean(-1).unsqueeze(-1)  # (N, C, H*W) -> (N, C)
-                # patch_sim = torch.abs(feature_crops - patch_mean).mean(dim=(-1, -2))  # for ID: .mean(dim=-2)
-
-                # gram matrix
-                feature_crops = feature_crops / torch.norm(feature_crops, 2, dim=1, keepdim=True)
-                feature_crops = torch.einsum("nlc,ncd->nld", feature_crops.permute((0,2,1)), feature_crops)
-
-                patch_mean = feature_crops.mean(dim=(-1,-2)).unsqueeze(-1).unsqueeze(-1)  # (N, C, H*W) -> (N, C)
+                patch_mean = feature_crops.mean(-1).unsqueeze(-1)  # (N, C, H*W) -> (N, C)
                 patch_sim = torch.abs(feature_crops - patch_mean).mean(dim=(-1, -2))  # for ID: .mean(dim=-2)
+
+                # # gram matrix
+                # feature_crops = feature_crops / torch.norm(feature_crops, 2, dim=1, keepdim=True)
+                # feature_crops = torch.einsum("nlc,ncd->nld", feature_crops.permute((0,2,1)), feature_crops)
+
+                # patch_mean = feature_crops.mean(dim=(-1,-2)).unsqueeze(-1).unsqueeze(-1)  # (N, C, H*W) -> (N, C)
+                # patch_sim = torch.abs(feature_crops - patch_mean).mean(dim=(-1, -2))  # for ID: .mean(dim=-2)
 
                 # c5_sum = feature_c5.sum(dim=[1,2,3])
                 # ratio = (c5_sum - 20000) / 10000
