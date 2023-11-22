4e7bc4c1932315644d37a4c09c2e818fdc1fecfa
ghw <haowenguo@tencent.com>
2023-11-22 19:50:38 +0800

    ood inference

---
+++ ood_inference/data_process.py
@@ -0,0 +1,35 @@
+import torchvision as tv
+from PIL import Image
+
+transform_imagenet = tv.transforms.Compose([
+    tv.transforms.Resize(256),
+    # tv.transforms.Resize(self.resize_size),
+    # tv.transforms.Resize(248, interpolation=tv.transforms.InterpolationMode.BICUBIC),
+    tv.transforms.CenterCrop(224),
+    # tv.transforms.CenterCrop(self.crop_size),
+    # tv.transforms.Resize((480, 480)),
+    tv.transforms.ToTensor(),
+    tv.transforms.Normalize([123.675/255, 116.28/255, 103.53/255],
+                            [58.395/255, 57.12/255, 57.375/255]),
+])
+
+transform_cifar = tv.transforms.Compose([
+    tv.transforms.Resize(32),
+    tv.transforms.CenterCrop(32),
+    tv.transforms.ToTensor(),
+    tv.transforms.Normalize([0.4914, 0.4822, 0.4465],
+                            [0.2023, 0.1994, 0.2010]),
+    # tv.transforms.Normalize([129.304/255, 124.07/255, 112.434/255],
+    #                         [68.17/255, 65.392/255, 70.418/255]),
+])
+
+
+def prepare_data(filename, transform):
+    results = dict()
+    sample = Image.open(filename)
+    if sample.mode != 'RGB':
+        sample = sample.convert('RGB')
+    sample = transform(sample)
+    results['img'] = sample
+    results['type'] = 3
+    return results
---
+++ ood_inference/fs_scaling.py
@@ -0,0 +1,16 @@
+from model_config import model as model_cfg
+from data_process import prepare_data, transform_imagenet
+
+import torch
+from mmcls.models import build_ood_model
+
+
+input = "/data/csxjiang/val/ILSVRC2012_val_00024946.JPEG"
+model = build_ood_model(model_cfg)
+model.init_weight()
+model.eval()
+
+with torch.no_grad():
+    x = prepare_data(input, transform_imagenet)
+    score, _ = model(x)
+    print(score)
