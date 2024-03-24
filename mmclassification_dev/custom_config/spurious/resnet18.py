1ebc5ab3afe9ffbf5fc3f68c64e5be3a20e3b24d
xuejiang <j763565607@gmail.com>
2024-03-24 23:25:26 +0800

    jx far celea spurious

--- mmclassification_dev/custom_config/spurious/resnet18.py
+++ mmclassification_dev/custom_config/spurious/resnet18.py
@@ -16,73 +16,74 @@ training_file = None
 
 # 18: (BasicBlock, (2, 2, 2, 2)),
 
-# model = dict(
-#     type=method_name,
-#     debug_mode=False,
-#     num_classes=2,
-#     # temperature=1,
-#     target_file=training_file,
-#     mode='mean',
-#     ood_detector=dict(
-#         type=method_list[0],
-#         classifier=dict(
-#         type='ImageClassifier',
-#         init_cfg=dict(type='Pretrained', checkpoint='/home/csxjiang/jx/sync/Spurious_OOD/checkpoints/waterbird/erm_r_0_9/erm_r_0_9_20230115/ckpt30.pth'),
-#         backbone=dict(
-#             type='ResNetActivation',
-#             depth=18,
-#             num_stages=4,
-#             out_indices=(3,),
-#             style='pytorch',
-#             th_act_k=0.1,
-#             th_act_stage=0,  ## 0:C2 1:C3 2:C4 3:C5
-#             th_act_location=1, ## No. of conv layer
-#             feature_sim_stage=1,  ## 0:C2 1:C3 2:C4 3:C5
-#             feature_sim_location=1,  ## No. of conv layer
-#         ),
-#     )
-#     )
-# )
-
 model = dict(
     type=method_name,
     debug_mode=False,
     num_classes=2,
     # temperature=1,
-    classifier=dict(
+    target_file=training_file,
+    mode='mean',
+    ood_detector=dict(
+        type=method_list[0],
+        classifier=dict(
         type='ImageClassifier',
-        init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/spurious_ood/celeba/resnet18_30e.pth'),
         # init_cfg=dict(type='Pretrained', checkpoint='/home/csxjiang/jx/sync/Spurious_OOD/checkpoints/waterbird/erm_r_0_9/erm_r_0_9_20230115/ckpt30.pth'),
+        init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/spurious_ood/celeba/resnet18_30e.pth'),
         backbone=dict(
             type='ResNetActivation',
             depth=18,
             num_stages=4,
             out_indices=(3,),
-            style='pytorch'),
-        neck=dict(type='GlobalAveragePooling'),
-        head=dict(
-            type='LinearClsHead',
-            num_classes=2,
-            in_channels=512,
-            loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
-            topk=(1, 5))
-        # head=dict(
-        #     type='ReactHead',
-        #     num_classes=2,
-        #     in_channels=512,
-        #     threshold=1.5,
-        #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
-        #     topk=(1, 5))
-        # head=dict(
-        #     type='DiceHead',
-        #     num_classes=2,
-        #     in_channels=512,
-        #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
-        #     topk=(1, 5),
-        #     info=info,
-        #     p=0.9,)
+            style='pytorch',
+            th_act_k=0.1,
+            th_act_stage=0,  ## 0:C2 1:C3 2:C4 3:C5
+            th_act_location=1, ## No. of conv layer
+            feature_sim_stage=1,  ## 0:C2 1:C3 2:C4 3:C5
+            feature_sim_location=1,  ## No. of conv layer
+        ),
+    )
     )
 )
+
+# model = dict(
+#     type=method_name,
+#     debug_mode=False,
+#     num_classes=2,
+#     # temperature=1,
+#     classifier=dict(
+#         type='ImageClassifier',
+#         init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/spurious_ood/celeba/resnet18_30e.pth'),
+#         # init_cfg=dict(type='Pretrained', checkpoint='/home/csxjiang/jx/sync/Spurious_OOD/checkpoints/waterbird/erm_r_0_9/erm_r_0_9_20230115/ckpt30.pth'),
+#         backbone=dict(
+#             type='ResNetActivation',
+#             depth=18,
+#             num_stages=4,
+#             out_indices=(3,),
+#             style='pytorch'),
+#         neck=dict(type='GlobalAveragePooling'),
+#         head=dict(
+#             type='LinearClsHead',
+#             num_classes=2,
+#             in_channels=512,
+#             loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+#             topk=(1, 5))
+#         # head=dict(
+#         #     type='ReactHead',
+#         #     num_classes=2,
+#         #     in_channels=512,
+#         #     threshold=1.5,
+#         #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+#         #     topk=(1, 5))
+#         # head=dict(
+#         #     type='DiceHead',
+#         #     num_classes=2,
+#         #     in_channels=512,
+#         #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+#         #     topk=(1, 5),
+#         #     info=info,
+#         #     p=0.9,)
+#     )
+# )
 # pipline =[dict(type='Collect', keys=['img'])]
 ood_pipeline =[dict(type='Collect', keys=['img', 'type'])]
 transform = "ImageNet"
