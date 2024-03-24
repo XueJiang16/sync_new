a55c2ca75078b082dd104e7d615445f2c559d0ce
xuejiang <j763565607@gmail.com>
2024-03-24 23:13:04 +0800

    jx far celea spurious

--- mmclassification_dev/custom_config/spurious/resnet18.py
+++ mmclassification_dev/custom_config/spurious/resnet18.py
@@ -1,6 +1,6 @@
 import os
 #
-info=os.path.expanduser('/data/csxjiang/dice_cache/spurious_resnet_p90_feature_stat.pth')
+# info=os.path.expanduser('/data/csxjiang/dice_cache/spurious_resnet_p90_feature_stat.pth')
 
 method_list = ['MSP', 'ODIN', 'Energy', 'GradNormBatch', 'FeatureReweight']
 method_name = method_list[2]
@@ -49,10 +49,10 @@ model = dict(
     debug_mode=False,
     num_classes=2,
     # temperature=1,
-    # target_file='/data/csxjiang/meta/train_LT_a8.txt',
     classifier=dict(
         type='ImageClassifier',
-        init_cfg=dict(type='Pretrained', checkpoint='/home/csxjiang/jx/sync/Spurious_OOD/checkpoints/waterbird/erm_r_0_9/erm_r_0_9_20230115/ckpt30.pth'),
+        init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/spurious_ood/celeba/resnet18_30e.pth'),
+        # init_cfg=dict(type='Pretrained', checkpoint='/home/csxjiang/jx/sync/Spurious_OOD/checkpoints/waterbird/erm_r_0_9/erm_r_0_9_20230115/ckpt30.pth'),
         backbone=dict(
             type='ResNetActivation',
             depth=18,
@@ -60,12 +60,12 @@ model = dict(
             out_indices=(3,),
             style='pytorch'),
         neck=dict(type='GlobalAveragePooling'),
-        # head=dict(
-        #     type='LinearClsHead',
-        #     num_classes=2,
-        #     in_channels=512,
-        #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
-        #     topk=(1, 5))
+        head=dict(
+            type='LinearClsHead',
+            num_classes=2,
+            in_channels=512,
+            loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+            topk=(1, 5))
         # head=dict(
         #     type='ReactHead',
         #     num_classes=2,
@@ -73,14 +73,14 @@ model = dict(
         #     threshold=1.5,
         #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
         #     topk=(1, 5))
-        head=dict(
-            type='DiceHead',
-            num_classes=2,
-            in_channels=512,
-            loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
-            topk=(1, 5),
-            info=info,
-            p=0.9,)
+        # head=dict(
+        #     type='DiceHead',
+        #     num_classes=2,
+        #     in_channels=512,
+        #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        #     topk=(1, 5),
+        #     info=info,
+        #     p=0.9,)
     )
 )
 # pipline =[dict(type='Collect', keys=['img'])]
@@ -88,53 +88,74 @@ ood_pipeline =[dict(type='Collect', keys=['img', 'type'])]
 transform = "ImageNet"
 transform2 = 'Cifar'
 
-# aug =
+
 aug = None
 
 data = dict(
     samples_per_gpu=256 if method_name is not 'ODIN' else 32,
     workers_per_gpu=4,
+    # id_data=dict(
+    #     name='waterbird',
+    #     type='CsvDataset',
+    #     path='/data/csxjiang/spurious_ood/waterbird_complete90_forest2water2',
+    #     pipeline=ood_pipeline,
+    #     # len_limit=5000 if quick_test else -1,
+    #     aug=aug,
+    # ),
     id_data=dict(
-        name='waterbird',
+        name='celea',
         type='CsvDataset',
-        path='/data/csxjiang/spurious_ood/waterbird_complete90_forest2water2',
+        path='/home/jx/sync/Spurious_OOD/datasets/celebA',
         pipeline=ood_pipeline,
         # len_limit=5000 if quick_test else -1,
         aug=aug,
+        transform='CelebA',
     ),
     ood_data=[
+        # dict(
+        #     name='SVHN',
+        #     type='FolderDataset',
+        #     path='/data/csxjiang/cifar_benchmark/svhn/images',
+        #     pipeline=ood_pipeline,
+        #     transform=transform,
+        #     len_limit=2000 if quick_test else -1,
+        # ),
+        # dict(
+        #     name='LSUN',
+        #     type='FolderDataset',
+        #     path='/data/csxjiang/cifar_benchmark/LSUN/test',
+        #     pipeline=ood_pipeline,
+        #     transform=transform,
+        #     len_limit=2000 if quick_test else -1,
+        # ),
+        # dict(
+        #     name='iSUN',
+        #     type='FolderDataset',
+        #     path='/data/csxjiang/cifar_benchmark/iSUN/iSUN_patches',
+        #     pipeline=ood_pipeline,
+        #     transform=transform,
+        #     len_limit = 2000 if quick_test else -1,
+        # ),
+        # dict(
+        #         name='Placebg',
+        #         type='FolderDataset',
+        #         path='/data/csxjiang/spurious_ood/ood_datasets/placesbg/image_collate',
+        #         pipeline=ood_pipeline,
+        #         len_limit=2000 if quick_test else -1,
+        #         aug=aug,
+        #     ),
         dict(
-            name='SVHN',
-            type='FolderDataset',
-            path='/data/csxjiang/cifar_benchmark/svhn/images',
-            pipeline=ood_pipeline,
-            transform=transform,
-            len_limit=2000 if quick_test else -1,
-        ),
-        dict(
-            name='LSUN',
-            type='FolderDataset',
-            path='/data/csxjiang/cifar_benchmark/LSUN/test',
-            pipeline=ood_pipeline,
-            transform=transform,
-            len_limit=2000 if quick_test else -1,
-        ),
-        dict(
-            name='iSUN',
-            type='FolderDataset',
-            path='/data/csxjiang/cifar_benchmark/iSUN/iSUN_patches',
-            pipeline=ood_pipeline,
-            transform=transform,
-            len_limit = 2000 if quick_test else -1,
-        ),
-        dict(
-                name='Placebg',
-                type='FolderDataset',
-                path='/data/csxjiang/spurious_ood/ood_datasets/placesbg/image_collate',
+                name='celea_ood',
+                type='CsvDataset',
+                path='/home/jx/sync/Spurious_OOD/datasets/celebA',
+                csv_name='celebA_ood.csv',
+                mode='val',
                 pipeline=ood_pipeline,
-                len_limit=2000 if quick_test else -1,
+                # len_limit=5000 if quick_test else -1,
                 aug=aug,
-            ),
+            transform='CelebA',
+
+        ),
         # dict(
         #     name='Places',
         #     type='FolderDataset',
