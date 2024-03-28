4ed5cffb87828835f9a0e9b903c5469261668692
ghw <haowenguo@tencent.com>
2024-03-28 23:55:13 +0800

    far on textures

--- mmclassification_dev/custom_config/dtd_resnet50.py
+++ mmclassification_dev/custom_config/dtd_resnet50.py
@@ -22,10 +22,7 @@ model = dict(
         type=method_list[0],
         classifier=dict(
         type='ImageClassifier',
-        init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/pytorch_official/resnet50_custom.pth'),
-        # init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/mmcls_offical/resnet50_8xb32_in1k_20210831-ea4938fc.pth'),
-        # init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/mmcls_offical/resnet34_8xb32_in1k_20210831-f257d4e6.pth'),
-        # init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/mmcls_offical/resnet101_8xb32_in1k_20210831-539c63f8.pth'),
+        init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/textures_ckpt/epoch_36.pth'),
         backbone=dict(
             type='ResNetActivation',
             depth=50,
