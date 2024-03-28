75688286db93a87e57790826a14c350c7c827855
ghw <haowenguo@tencent.com>
2024-03-28 23:48:12 +0800

    far on textures

--- mmclassification_dev/custom_config/dtd_resnet50.py
+++ mmclassification_dev/custom_config/dtd_resnet50.py
@@ -12,28 +12,26 @@ if custom_name is not None:
 else:
     readable_name ='{}_{}_{}'.format(method_name, model_name, train_dataset)
 quick_test = False
-# training_file = '/data/csxjiang/meta/train_labeled.txt'
 model = dict(
     type=method_name,
-    debug_mode=False,
+    debug_mode=True,
     num_classes=64,
-    # temperature=1,
-    # target_file='/data/csxjiang/meta/train_LT_a8.txt',
-    classifier=dict(
+    mode='mean',
+    ood_detector=dict(
+        type=method_list[0],
+        classifier=dict(
         type='ImageClassifier',
-        init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/textures_ckpt/epoch_36.pth'),
+        init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/pytorch_official/resnet50_custom.pth'),
+        # init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/mmcls_offical/resnet50_8xb32_in1k_20210831-ea4938fc.pth'),
+        # init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/mmcls_offical/resnet34_8xb32_in1k_20210831-f257d4e6.pth'),
+        # init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/mmcls_offical/resnet101_8xb32_in1k_20210831-539c63f8.pth'),
         backbone=dict(
-            # type='ResNet',
-            # depth=50,
-            # num_stages=4,
-            # out_indices=(3,),
-            # style='pytorch'),
             type='ResNetActivation',
             depth=50,
             num_stages=4,
             out_indices=(3,),
             style='pytorch',
-            th_act_k=0.1,
+            th_act_k=0.2,
             th_act_stage=2,  ## 0:C2 1:C3 2:C4 3:C5
             th_act_location=5, ## No. of conv layer
             feature_sim_stage=3,  ## 0:C2 1:C3 2:C4 3:C5
@@ -46,21 +44,7 @@ model = dict(
         #     in_channels=2048,
         #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
         #     topk=(1, 5))
-        # head=dict(
-        #     type='DiceHead',
-        #     num_classes=1000,
-        #     in_channels=2048,
-        #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
-        #     topk=(1, 5),
-        #     info=info,
-        #     p=0.7,)
-        # head=dict(
-        #     type='ReactHead',
-        #     num_classes=1000,
-        #     in_channels=2048,
-        #     threshold=1,
-        #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
-        #     topk=(1, 5))
+    )
     )
 )
 pipline =[
