8be9b286886371c9341ba52fb2ddc1a98b0925fd
JJJUMP <j763565607@gmail.com>
2024-05-30 08:47:35 +0000

    Update file resnet50.py

---
+++ mmclassification_dev/custom_config/kd/resnet50.py
@@ -0,0 +1,120 @@
+# import os
+#
+# info=os.path.expanduser('/data/csxjiang/dice_cache/imagenet_res50_feature_stat.pth')
+method_list = ['MSP', 'ODIN', 'Energy', 'GradNormBatch']
+
+method_name = method_list[0]
+model_name = 'resnet50'
+train_dataset = 'Balance'
+custom_name = "Official"
+if custom_name is not None:
+    readable_name = '{}_{}_{}_{}'.format(method_name, model_name, train_dataset, custom_name)
+else:
+    readable_name ='{}_{}_{}'.format(method_name, model_name, train_dataset)
+quick_test = False
+# training_file = '/data/csxjiang/meta/train_labeled.txt'
+model = dict(
+    type=method_name,
+    debug_mode=False,
+    num_classes=1000,
+    classifier=dict(
+        type='ImageClassifier',
+        # init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/kd_ckpt/abloss/resnet50_8xb32_in1k_20210831-ea4938fc.pth'),
+        init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/pytorch_official/resnet50_custom.pth'),
+        backbone=dict(
+            type='ResNet',
+            depth=50,
+            num_stages=4,
+            out_indices=(3,),
+            style='pytorch'),
+        neck=dict(type='GlobalAveragePooling'),
+        head=dict(
+            type='LinearClsHead',
+            num_classes=1000,
+            in_channels=2048,
+            loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+            topk=(1, 5))
+        # head=dict(
+        #     type='DiceHead',
+        #     num_classes=1000,
+        #     in_channels=2048,
+        #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        #     topk=(1, 5),
+        #     info=info,
+        #     p=0.7,)
+        # head=dict(
+        #     type='ReactHead',
+        #     num_classes=1000,
+        #     in_channels=2048,
+        #     threshold=1,
+        #     loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        #     topk=(1, 5))
+    )
+)
+pipline =[
+          dict(type='Collect', keys=['img', 'type'])
+]
+data = dict(
+    samples_per_gpu=32,
+    workers_per_gpu=4,
+    id_data=dict(
+        name='ImageNet',
+        type='TxtDataset',
+        path='/data/csxjiang/val',
+        data_ann='/data/csxjiang/meta/val_labeled.txt',
+        # path='/data/csxjiang/ILSVRC/Data/CLS-LOC/train',
+        # data_ann='/data/csxjiang/meta/train_labeled.txt',
+        pipeline=pipline,
+        len_limit=5000 if quick_test else -1,
+    ),
+    # id_data=dict(
+    #     type='JsonDataset',
+    #     path='/data/csxjiang/',
+    #     data_ann='/data/csxjiang/ood_data/inat/val2018.json',
+    #     pipeline=[
+    #         dict(type='LoadImageFromFile'),
+    #         dict(type='Resize', size=480),
+    #         dict(
+    #             type='Normalize',
+    #             mean=[123.675, 116.28, 103.53],
+    #             std=[58.395, 57.12, 57.375],
+    #             to_rgb=True),
+    #         dict(type='ImageToTensor', keys=['img']),
+    #         dict(type='Collect', keys=['img'])
+    #     ]),
+    ood_data=[
+        dict(
+            name='iNaturalist',
+            type='FolderDataset',
+            path='/data/csxjiang/ood_data/iNaturalist/images',
+            pipeline=pipline,
+            len_limit=1000 if quick_test else -1,
+        ),
+        dict(
+            name='SUN',
+            type='FolderDataset',
+            path='/data/csxjiang/ood_data/SUN/images',
+            pipeline=pipline,
+            len_limit=1000 if quick_test else -1,
+        ),
+        dict(
+            name='Places',
+            type='FolderDataset',
+            path='/data/csxjiang/ood_data/Places/images',
+            pipeline=pipline,
+            len_limit=1000 if quick_test else -1,
+        ),
+        dict(
+            name='Textures',
+            type='FolderDataset',
+            path='/data/csxjiang/ood_data/Textures/dtd/images_collate',
+            pipeline=pipline,
+            len_limit=1000 if quick_test else -1,
+        ),
+    ],
+
+)
+dist_params = dict(backend='nccl')
+log_level = 'CRITICAL'
+# log_level = 'INFO'
+work_dir = './results'
