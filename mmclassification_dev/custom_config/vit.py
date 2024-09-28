26beaab595ccd2f322eb8ace830cc0cae65e5692
JJJUMP <j763565607@gmail.com>
2024-09-28 14:23:34 +0000

    Update file vit.py

--- mmclassification_dev/custom_config/vit.py
+++ mmclassification_dev/custom_config/vit.py
@@ -1,7 +1,7 @@
 import os
 
 method_list = ["GradNorm", "MSP", "Energy", "ODIN", 'FeatureReweight']
-method_name = method_list[1]
+method_name = method_list[-1]
 model_name = 'vit'
 # info=os.path.expanduser('/data/csxjiang/dice_cache/imagenet_res101_{}_feature_stat.pth'.format(train_dataset))
 
@@ -14,39 +14,12 @@ quick_test = False
 # training_file = None
 
 # for far score
-# model = dict(
-#     type=method_name,
-#     num_classes=1000,
-#     mode='vit',
-#     ood_detector=dict(
-#     type=method_list[1],
-#     classifier= dict(
-#     type='ImageClassifier',
-#     init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/mmcls_offical/vit-base-p16_in21k-pre-3rdparty_ft-64xb64_in1k-384_20210928-98e8652b.pth'),
-#     backbone=dict(
-#     type='VisionTransformer',
-#     arch='b',
-#     img_size=384,
-#     patch_size=16,
-#     output_cls_token=False,
-#     out_indices=10,
-#     # drop_rate=0.1,
-#     ),
-#     # neck=None,
-#     # head=dict(
-#     #     type='VisionTransformerClsHead',
-#     #     num_classes=1000,
-#     #     in_channels=768,
-#     #     loss=dict(
-#     #         type='LabelSmoothLoss', label_smooth_val=0.1,
-#     #         mode='classy_vision'),)
-#     ))
-# )
-
-# for baseline
 model = dict(
     type=method_name,
     num_classes=1000,
+    mode='vit',
+    ood_detector=dict(
+    type=method_list[1],
     classifier= dict(
     type='ImageClassifier',
     init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/mmcls_offical/vit-base-p16_in21k-pre-3rdparty_ft-64xb64_in1k-384_20210928-98e8652b.pth'),
@@ -55,18 +28,45 @@ model = dict(
     arch='b',
     img_size=384,
     patch_size=16,
+    output_cls_token=False,
+    out_indices=10,
     # drop_rate=0.1,
     ),
-    neck=None,
-    head=dict(
-        type='VisionTransformerClsHead',
-        num_classes=1000,
-        in_channels=768,
-        loss=dict(
-            type='LabelSmoothLoss', label_smooth_val=0.1,
-            mode='classy_vision'),
+    # neck=None,
+    # head=dict(
+    #     type='VisionTransformerClsHead',
+    #     num_classes=1000,
+    #     in_channels=768,
+    #     loss=dict(
+    #         type='LabelSmoothLoss', label_smooth_val=0.1,
+    #         mode='classy_vision'),)
     ))
 )
+
+# for baseline
+# model = dict(
+#     type=method_name,
+#     num_classes=1000,
+#     classifier= dict(
+#     type='ImageClassifier',
+#     init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/mmcls_offical/vit-base-p16_in21k-pre-3rdparty_ft-64xb64_in1k-384_20210928-98e8652b.pth'),
+#     backbone=dict(
+#     type='VisionTransformer',
+#     arch='b',
+#     img_size=384,
+#     patch_size=16,
+#     # drop_rate=0.1,
+#     ),
+#     neck=None,
+#     head=dict(
+#         type='VisionTransformerClsHead',
+#         num_classes=1000,
+#         in_channels=768,
+#         loss=dict(
+#             type='LabelSmoothLoss', label_smooth_val=0.1,
+#             mode='classy_vision'),
+#     ))
+# )
 
 
 # pipline =[dict(type='Collect', keys=['img'])]
