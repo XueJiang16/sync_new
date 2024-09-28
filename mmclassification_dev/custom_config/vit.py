4a1584d2120b9316fc783013ea2c26037d4b5e80
JJJUMP <j763565607@gmail.com>
2024-09-28 13:56:56 +0000

    Update file vit.py

--- mmclassification_dev/custom_config/vit.py
+++ mmclassification_dev/custom_config/vit.py
@@ -12,12 +12,41 @@ else:
     readable_name ='{}_{}'.format(method_name, model_name)
 quick_test = False
 # training_file = None
+
+# for far score
+# model = dict(
+#     type=method_name,
+#     num_classes=1000,
+#     mode='vit',
+#     ood_detector=dict(
+#     type=method_list[1],
+#     classifier= dict(
+#     type='ImageClassifier',
+#     init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/mmcls_offical/vit-base-p16_in21k-pre-3rdparty_ft-64xb64_in1k-384_20210928-98e8652b.pth'),
+#     backbone=dict(
+#     type='VisionTransformer',
+#     arch='b',
+#     img_size=384,
+#     patch_size=16,
+#     output_cls_token=False,
+#     out_indices=10,
+#     # drop_rate=0.1,
+#     ),
+#     # neck=None,
+#     # head=dict(
+#     #     type='VisionTransformerClsHead',
+#     #     num_classes=1000,
+#     #     in_channels=768,
+#     #     loss=dict(
+#     #         type='LabelSmoothLoss', label_smooth_val=0.1,
+#     #         mode='classy_vision'),)
+#     ))
+# )
+
+# for baseline
 model = dict(
     type=method_name,
     num_classes=1000,
-    mode='vit',
-    ood_detector=dict(
-    type=method_list[1],
     classifier= dict(
     type='ImageClassifier',
     init_cfg=dict(type='Pretrained', checkpoint='/data/csxjiang/ood_ckpt/mmcls_offical/vit-base-p16_in21k-pre-3rdparty_ft-64xb64_in1k-384_20210928-98e8652b.pth'),
@@ -26,8 +55,6 @@ model = dict(
     arch='b',
     img_size=384,
     patch_size=16,
-    output_cls_token=False,
-    out_indices=10,
     # drop_rate=0.1,
     ),
     neck=None,
@@ -37,9 +64,10 @@ model = dict(
         in_channels=768,
         loss=dict(
             type='LabelSmoothLoss', label_smooth_val=0.1,
-            mode='classy_vision'),)
+            mode='classy_vision'),
     ))
 )
+
 
 # pipline =[dict(type='Collect', keys=['img'])]
 pipline =[dict(type='Collect', keys=['img', 'type'])]
