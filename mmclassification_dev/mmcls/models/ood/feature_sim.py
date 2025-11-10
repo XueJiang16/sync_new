f8988e68c9a323bc1ae287aa5a15141a3ba4172e
Xue Jiang <40789479+XueJiang16@users.noreply.github.com>
2025-11-10 14:51:40 +0800

    1

--- mmclassification_dev/mmcls/models/ood/feature_sim.py
+++ mmclassification_dev/mmcls/models/ood/feature_sim.py
@@ -242,7 +242,7 @@ class FeatureReweight(BaseModule):
                 patch_sim = torch.abs(feature_crops - patch_mean).mean(dim=(-1, -2))  # for ID: .mean(dim=-2)
 
                 # patch_sim = msp_scores + (patch_sim * 0.002) 
-                patch_sim = energy + (patch_sim * 0.005) 
+                patch_sim = energy + (patch_sim * 0.003) 
 
 
                 # target_layer = self.ood_detector.classifier.backbone.layers[11]
